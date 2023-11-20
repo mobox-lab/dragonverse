13b1e78e7f7f20a288e5c374474a05b0046ef1f3
joji-mb <545633740@qq.com>
2023-11-20 16:23:35 +0800

    feat: update BagItem

---
+++ JavaScripts/config/BagItem.ts
@@ -0,0 +1,20 @@
+import { ConfigBase, IElementBase } from "./ConfigBase";
+const EXCELDATA:Array<Array<any>> = [["id","name","desc","stackCount","icon"],["","Language","","",""],[1,"BagItemName0001","测试用物品",64,"222920"]];
+export interface IBagItemElement extends IElementBase{
+ 	/**背包物品 ID*/
+	id:number
+	/**物品名称*/
+	name:string
+	/**物品描述*/
+	desc:string
+	/**最大堆叠数量*/
+	stackCount:number
+	/**icon*/
+	icon:string
+ } 
+export class BagItemConfig extends ConfigBase<IBagItemElement>{
+	constructor(){
+		super(EXCELDATA);
+	}
+
+}
