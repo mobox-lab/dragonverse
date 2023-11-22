c3ac96cbfdfbc10e5636e6409d3f2c702382c852
joji-mb <545633740@qq.com>
2023-11-22 15:14:27 +0800

    feat: export Dragon Config

---
+++ JavaScripts/config/Dragon.ts
@@ -0,0 +1,34 @@
+import { ConfigBase, IElementBase } from "./ConfigBase";
+const EXCELDATA:Array<Array<any>> = [["id","name","bagId","avatar","elementalId","qualityId","generationAreaId","existenceTime","generationInterval","hitPoint","cost","successRateAlgoId"],["","","","","","","","","","","",""]];
+export interface IDragonElement extends IElementBase{
+ 	/**龙 ID*/
+	id:number
+	/**名称*/
+	name:string
+	/**背包物 ID*/
+	bagId:number
+	/**形象*/
+	avatar:number
+	/**元素 ID*/
+	elementalId:number
+	/**品质 ID*/
+	qualityId:number
+	/**生成区域 ID*/
+	generationAreaId:Array<number>
+	/**存在时间*/
+	existenceTime:number
+	/**生成间隔*/
+	generationInterval:number
+	/**可捕捉次数*/
+	hitPoint:number
+	/**捕捉消耗*/
+	cost:number
+	/**捕捉成功率算法*/
+	successRateAlgoId:number
+ } 
+export class DragonConfig extends ConfigBase<IDragonElement>{
+	constructor(){
+		super(EXCELDATA);
+	}
+
+}
