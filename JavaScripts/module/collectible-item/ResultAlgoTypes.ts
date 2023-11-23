76fa3159c35fc31afdeaf476c20aeac3d22f562c
joji-mb <545633740@qq.com>
2023-11-23 18:03:20 +0800

    style: restructure ResultAlgoTypes

--- JavaScripts/module/collectible-item/ResultAlgoTypes.ts
+++ JavaScripts/module/collectible-item/ResultAlgoTypes.ts
@@ -6,12 +6,18 @@ export enum ResultAlgoTypes {
      * 空置.
      */
     Null,
+    /**
+     * 测试结果算法.
+     */
+    TestAlgo
 }
 
 export type ResultAlgo = (...param: unknown[]) => number;
 
 export function ResultAlgoFactory(type: ResultAlgoTypes): ResultAlgo {
     switch (type) {
+        case ResultAlgoTypes.TestAlgo:
+            return TestAlgo;
         case ResultAlgoTypes.Null:
         default:
             return NormalResultAlgo;
@@ -20,4 +26,8 @@ export function ResultAlgoFactory(type: ResultAlgoTypes): ResultAlgo {
 
 export function NormalResultAlgo() {
     return 10;
+}
+
+export function TestAlgo(): number {
+    return 100;
 }
