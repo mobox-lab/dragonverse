136b0d24015a348a8642f19907aa3bcfd58c4af5
joji-mb <545633740@qq.com>
2023-11-27 18:47:50 +0800

    feat: add handbook uis

---
+++ JavaScripts/ui/handbook/HandbookPanel.ts
@@ -0,0 +1,70 @@
+import HandbookPanel_Generate from "../../ui-generate/handbook/HandbookPanel_generate";
+import { BagItemUnique, BagModuleC, HandbookItemUnique } from "../../module/bag/BagModule";
+import ScrollView from "../../depend/scroll-view/ScrollView";
+import BagPanelItem from "../bag/BagPanelItem";
+import HandbookPanelItem from "./HandbookPanelItem";
+import GToolkit from "../../util/GToolkit";
+import { Yoact } from "../../depend/yoact/Yoact";
+import bindYoact = Yoact.bindYoact;
+
+export default class HandbookPanel extends HandbookPanel_Generate {
+//#region Member
+    private _bagModule: BagModuleC;
+    private _scrollView: ScrollView<HandbookItemUnique, HandbookPanelItem>;
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region MetaWorld UI Event
+
+    protected onAwake(): void {
+        super.onAwake();
+        this.canUpdate = true;
+
+//#region Member init
+        this.btnClose.onClicked.add(
+            () => UIService.hide(HandbookPanel),
+        );
+
+        this._bagModule = ModuleService.getModule(BagModuleC);
+        this._scrollView = new ScrollView<HandbookItemUnique, HandbookPanelItem>(
+            this._bagModule.handbookYoact,
+            HandbookPanelItem,
+            this.mScrollView,
+            undefined,
+            true,
+        ).listenOnItemSelect((key: number) => {
+            GToolkit.log(HandbookPanel, `item clicked. id: ${key}`);
+        });
+
+        bindYoact(() => {
+            this.mTextCompletion.text=`/${this._bagModule.handbookYoact}`
+        })
+//#endregion ------------------------------------------------------------------------------------------
+
+//#region Widget bind
+//#endregion ------------------------------------------------------------------------------------------
+
+//#region Event subscribe
+//#endregion ------------------------------------------------------------------------------------------
+    }
+
+    protected onUpdate() {
+    }
+
+    protected onShow() {
+        this._scrollView.resetSelect();
+    }
+
+    protected onHide() {
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Init
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region UI Behavior
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Event Callback
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
