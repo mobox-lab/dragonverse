5d0670721dc8fa7b3326cb6845cfe417a1b5c289
joji-mb <545633740@qq.com>
2023-11-30 19:08:02 +0800

    feat: update SDB add sd ai

--- JavaScripts/module/scene-dragon/SceneDragonBehavior.ts
+++ JavaScripts/module/scene-dragon/SceneDragonBehavior.ts
@@ -2,9 +2,100 @@ import GToolkit from "../../util/GToolkit";
 import SceneDragon from "./SceneDragon";
 import Character = mw.Character;
 import GameServiceConfig from "../../const/GameServiceConfig";
-import { SceneDragonModuleC } from "./SceneDragonModule";
 import { EventDefine } from "../../const/EventDefine";
 import Regulator from "../../depend/regulator/Regulator";
+import FiniteStateMachine, { Region, State } from "../../depend/finite-state-machine/FiniteStateMachine";
+import { Yoact } from "../../depend/yoact/Yoact";
+import createYoact = Yoact.createYoact;
+import bindYoact = Yoact.bindYoact;
+
+class SceneDragonBehaviorState {
+//#region Constant
+    /**
+     * 闲置体力恢复速率. /s
+     */
+    public static readonly IDLE_STAMINA_RECOVERY = 10;
+
+    /**
+     * 闲置时 活跃体力恢复速率. /s
+     */
+    public static readonly ACTIVE_STAMINA_RECOVERY_IN_IDLE = 10;
+
+    /**
+     * 奔跑时 活跃体力恢复速率. /s
+     */
+    public static readonly ACTIVE_STAMINA_RECOVERY_IN_RUNNING = -10;
+
+    /**
+     * 行走时 活跃体力恢复速率. /s
+     */
+    public static readonly ACTIVE_STAMINA_RECOVERY_IN_WALKING = 5;
+
+    /**
+     * 坐立难安 中彩率.
+     * @desc 当坐立难安时 {@link ACTIVE_STAMINA_RECOVERY_IN_WALKING} 作为正数使用.
+     * @desc 否则 {@link ACTIVE_STAMINA_RECOVERY_IN_WALKING} 作为负数使用.
+     */
+    public static readonly RESTLESS_RATIO = 0.5;
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    /**
+     * 闲置体力.
+     * @desc 随时间回复.
+     * @desc 决定待机动作.
+     */
+    public idleStamina: number;
+
+    /**
+     * 活跃体力.
+     * @desc 随时间回复.
+     * @desc 决定活跃动作.
+     */
+    public activeStamina: number;
+
+    /**
+     * 是否 存活.
+     */
+    public alive: boolean = true;
+
+    /**
+     * 是否 好动的.
+     */
+    public restless: boolean = false;
+
+    /**
+     * 寻路目的地.
+     */
+    public destination: Vector = null;
+
+    constructor(idleStamina: number = 0, activeStamina: number = 0) {
+        this.idleStamina = idleStamina;
+        this.activeStamina = activeStamina;
+    }
+}
+
+enum SceneDragonStates {
+    /**
+     * 原地呼吸.
+     */
+    IdleWait = "idle wait",
+    /**
+     * 休闲动作.
+     */
+    IdleMotion = "idle motion",
+    /**
+     * 行走.
+     */
+    Walk = "walk",
+    /**
+     * 奔跑.
+     */
+    Run = "run",
+    /**
+     * 死亡.
+     */
+    Death = "death"
+}
 
 @Component
 export default class SceneDragonBehavior extends mw.Script {
@@ -22,6 +113,10 @@ export default class SceneDragonBehavior extends mw.Script {
         }
         return this._character;
     }
+
+    public state: SceneDragonBehaviorState = createYoact(new SceneDragonBehaviorState());
+
+    private _machine: FiniteStateMachine<any>;
 
 //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
@@ -52,10 +147,17 @@ export default class SceneDragonBehavior extends mw.Script {
             GToolkit.log(SceneDragonBehavior, `dragon out of alive range. syncKey: ${this.syncKey}`);
             Event.dispatchToLocal(EventDefine.DragonOutOfAliveRange, this.syncKey);
         }
+
+        this._machine.update(dt);
+        this.state.idleStamina += dt * SceneDragonBehaviorState.IDLE_STAMINA_RECOVERY;
+
     }
 
     protected onDestroy(): void {
         super.onDestroy();
+
+        this.state.alive = false;
+        this.gameObject.destroy();
 
 //#region Event subscribe
         //TODO_LviatYi 
@@ -68,6 +170,8 @@ export default class SceneDragonBehavior extends mw.Script {
     public init(syncKey: string, data: SceneDragon) {
         this.syncKey = syncKey;
         this.data = data;
+
+        this.initStateMachine();
     }
 
 //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
@@ -76,6 +180,125 @@ export default class SceneDragonBehavior extends mw.Script {
     private checkAlive(): boolean {
         return Vector.squaredDistance(this.gameObject.worldTransform.position, this.character.worldTransform.position) <
             GameServiceConfig.SQR_SCENE_DRAGON_MAX_LIVE_DISTANCE;
+    }
+
+    private initStateMachine() {
+        const idleWait = new State<SceneDragonBehaviorState>(SceneDragonStates.IdleWait)
+            .aE(() => {
+                GToolkit.log(SceneDragonBehavior,
+                    `enter idle wait state.`,
+                    `key: ${this.syncKey}.`);
+                if (this.state.destination) {
+                    this.state.destination = null;
+                    Navigation.stopNavigateTo(this.gameObject);
+                }
+            })
+            .aU((dt) => this.state.activeStamina += dt * SceneDragonBehaviorState.ACTIVE_STAMINA_RECOVERY_IN_IDLE);
+        const idleMotion = new State<SceneDragonBehaviorState>(SceneDragonStates.IdleMotion)
+            .aE(() => {
+                GToolkit.log(SceneDragonBehavior,
+                    `enter idle motion state.`,
+                    `key: ${this.syncKey}.`);
+                if (this.state.destination) {
+                    this.state.destination = null;
+                    Navigation.stopNavigateTo(this.gameObject);
+                }
+                setTimeout(
+                    () => this.state.idleStamina -= 100,
+                    1e3,
+                );
+            });
+        const walk = new State<SceneDragonBehaviorState>(SceneDragonStates.Walk)
+            .aE(() => {
+                GToolkit.log(SceneDragonBehavior,
+                    `enter walk state.`,
+                    `key: ${this.syncKey}.`);
+                (this.gameObject as Character).maxWalkSpeed = SceneDragon.WALK_SPEED;
+                this.state.restless = Math.random() < SceneDragonBehaviorState.RESTLESS_RATIO;
+            })
+            .aU((dt) => {
+                this.tryMoveRandom();
+                this.state.activeStamina +=
+                    dt
+                    * (this.state.restless ? 1 : -1)
+                    * (SceneDragonBehaviorState.ACTIVE_STAMINA_RECOVERY_IN_WALKING);
+            });
+        const run = new State<SceneDragonBehaviorState>(SceneDragonStates.Run)
+            .aE(() => {
+                GToolkit.log(SceneDragonBehavior,
+                    `enter run state.`,
+                    `key: ${this.syncKey}.`);
+                (this.gameObject as Character).maxWalkSpeed = SceneDragon.RUN_SPEED;
+                this.state.restless = false;
+            })
+            .aU((dt) => {
+                this.tryMoveRandom();
+                this.state.activeStamina += dt * SceneDragonBehaviorState.ACTIVE_STAMINA_RECOVERY_IN_RUNNING;
+            });
+        const death = new State<SceneDragonBehaviorState>(SceneDragonStates.Death)
+            .aE(() => {
+                GToolkit.log(SceneDragonBehavior,
+                    `enter death state.`,
+                    `key: ${this.syncKey}.`);
+            });
+
+        const idle = new Region<SceneDragonBehaviorState>("idle").include(idleWait, idleMotion);
+        const active = new Region<SceneDragonBehaviorState>("active").include(walk, run);
+        const alive = new Region<SceneDragonBehaviorState>("alive").include(idleWait, idleMotion, walk, run);
+
+        idleWait.when((arg) => arg.alive && arg.activeStamina < 100 && arg.idleStamina > 100).to(idleMotion);
+        idleMotion.when((arg) => arg.idleStamina < 100).to(idleWait);
+        idle.when(arg => arg.activeStamina > 50).to(walk);
+        walk.when((arg) => arg.activeStamina > 100).to(run);
+        run.when((arg) => arg.activeStamina < 0).to(idle);
+        active.when(arg => arg.activeStamina < 0).to(idleWait);
+        alive.when(arg => !arg.alive).to(death);
+
+        this._machine = new FiniteStateMachine<SceneDragonBehaviorState>(idleWait);
+        bindYoact(() => {
+            this._machine.evaluate(this.state);
+        });
+    }
+
+    private getRandomDestination(): mw.Vector | undefined {
+        let randDist: number = GToolkit.random(SceneDragon.NAVIGATION_RANDOM_MIN_DISTANCE, SceneDragon.NAVIGATION_RANDOM_MAX_DISTANCE);
+        let randVec = Vector.rotateZ(Vector.forward, Vector.zero, GToolkit.random(0, Math.PI * 2)).multiply(randDist).add(this.gameObject.worldTransform.position);
+
+        randVec =
+            GToolkit.detectVerticalTerrain(
+                GToolkit.newWithZ(randVec, this.gameObject.worldTransform.position.z + 500),
+                3000,
+                this.gameObject,
+                undefined,
+                true,
+            )?.position;
+        if (randVec && GToolkit.hasValidPath(this.gameObject.worldTransform.position, randVec)) {
+            return randVec;
+        }
+        return undefined;
+    }
+
+    private tryMoveRandom() {
+        if (this.state.destination) {
+            return;
+        }
+
+        this.state.destination = this.getRandomDestination();
+        if (!this.state.destination) {
+            return;
+        }
+
+        GToolkit.log(SceneDragonBehavior, `navigating now.`,
+            `key: ${this.syncKey}`,
+            `destination: ${this.state.destination}`,
+        );
+        Navigation.navigateTo(
+            this.gameObject,
+            this.state.destination,
+            undefined,
+            () => GToolkit.log(SceneDragonBehavior, `navigate success`),
+            () => GToolkit.log(SceneDragonBehavior, `navigate fail`),
+        );
     }
 
 //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
