5c6d56169065b5a4d177c11172f62628672147e7
joji-mb <545633740@qq.com>
2023-12-07 20:52:36 +0800

    feat: add BeginnerGuard

---
+++ JavaScripts/module/auth/trigger/BeginnerGuard.ts
@@ -0,0 +1,125 @@
+import GToolkit from "../../../util/GToolkit";
+import Log4Ts from "../../../depend/log4ts/Log4Ts";
+import Character = mw.Character;
+import NpcBehavior from "../../npc/NpcBehavior";
+import { EventDefine } from "../../../const/EventDefine";
+import { NpcModuleC } from "../../npc/NpcModule";
+import DialogueManager from "../../../gameplay/dialogue/DialogueManager";
+
+/**
+ * 采集物 Trigger.
+ *
+ * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
+ * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
+ * ⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄
+ * ⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄
+ * ⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+ * @author LviatYi
+ */
+@Component
+export default class BeginnerGuard extends mw.Script {
+//#region Constant
+    /**
+     * 守卫 Npc Id.
+     * @private
+     */
+    private static readonly GUARD_NPC_ID: number = null;
+
+    /**
+     * 禁行区 Guid.
+     * @private
+     */
+    private static readonly FORBIDDEN_AREA_GUID: string = null;
+
+    /**
+     * 守卫警告对话内容节点 Id.
+     * @private
+     */
+    private static readonly GUARD_CONTENT_ID: number = 1;
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Member
+    private guardNpc: NpcBehavior = null;
+
+    private forbiddenArea: BlockingVolume = null;
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//region MetaWorld Event
+    protected onStart(): void {
+        super.onStart();
+
+        if (mw.SystemUtil.isServer()) {
+            return;
+        }
+//region Member init
+        this.guardNpc = ModuleService.getModule(NpcModuleC).getNpc(BeginnerGuard.GUARD_NPC_ID);
+        if (!this.guardNpc) {
+            Log4Ts.error(BeginnerGuard, `guard npc not found. Id: ${BeginnerGuard.GUARD_NPC_ID}`);
+        }
+
+        this.forbiddenArea = GameObject.findGameObjectById(BeginnerGuard.FORBIDDEN_AREA_GUID) as BlockingVolume;
+        if (!this.forbiddenArea) {
+            Log4Ts.error(BeginnerGuard, `forbidden area not found. Id: ${BeginnerGuard.FORBIDDEN_AREA_GUID}`);
+        }
+//endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//region Widget bind
+//endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//region Event subscribe
+        let gameObject: mw.GameObject = this.gameObject;
+        if (gameObject) {
+            let trigger = gameObject as mw.Trigger;
+            if (trigger) {
+                trigger.onEnter.add(this.onObjectEnter);
+            } else {
+                Log4Ts.error(BeginnerGuard, `there is no trigger under this game object.`);
+            }
+        }
+
+        Event.addLocalListener(EventDefine.PlayerEnableEnter, this.onPlayerEnableEnter);
+//endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+    }
+
+    protected onDestroy(): void {
+        super.onDestroy();
+    }
+
+//endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Method
+    public init(syncKey: string) {
+        this._syncKey = syncKey;
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//region Event Callback
+    private onObjectEnter = (obj: GameObject): void => {
+        if (GToolkit.isSelfCharacter(obj)) {
+            const playerId = (obj as Character).player.playerId;
+            this.characterEnter(playerId);
+        }
+    };
+
+
+    private characterEnter = (playerId: number) => {
+        Log4Ts.log(BeginnerGuard, `player enter. playerId: ${playerId}`);
+        if (playerId !== Player.localPlayer.playerId) return;
+
+        if (this.forbiddenArea.getTargetPassable(Player.localPlayer.character)) return;
+
+        if (BeginnerGuard.GUARD_CONTENT_ID === null) {
+            Log4Ts.error(BeginnerGuard, `guard content node not found. Id: ${BeginnerGuard.GUARD_CONTENT_ID}`);
+            return;
+        }
+        DialogueManager.getInstance().chat(BeginnerGuard.GUARD_CONTENT_ID, true);
+    };
+
+    private onPlayerEnableEnter = () => {
+        this.forbiddenArea.addPassableTarget(Player.localPlayer.character);
+    };
+
+//endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
