f5e54eaf6df302d2078d4fd5b5e2fff7c153cf71
joji-mb <545633740@qq.com>
2023-12-07 22:03:02 +0800

    chore: un trace L4T selfconfig

--- JavaScripts/depend/log4ts/Log4TsSelfConfig.ts
+++
@@ -1,8 +0,0 @@
-// recommend to add this file in gitignore
-
-import Log4Ts, { Log4TsConfig } from "./Log4Ts";
-
-Log4Ts.setConfig(
-    new Log4TsConfig()
-        .addBlackList("IDontWantThis"),
-);
