38c6b4004154b4dfa117b136b4c4850f98bd8770
Tomori6657 <japancjc123@gmail.com>
2023-12-12 17:30:45 +0800

    feat：npc交互动作

---
+++ JavaScripts/ui-generate/dialogue/NPCActionPanel_generate.ts
@@ -0,0 +1,44 @@
+﻿
+/**
+ * AUTO GENERATE BY UI EDITOR.
+ * WARNING: DO NOT MODIFY THIS FILE,MAY CAUSE CODE LOST.
+ * ATTENTION: onStart 等UI脚本自带函数不可改写为异步执行，有需求的异步逻辑请使用函数封装，通过函数接口在内部使用
+ * UI: UI/dialogue/NPCActionPanel.ui
+*/
+
+
+
+@UIBind('UI/dialogue/NPCActionPanel.ui')
+export default class NPCActionPanel_Generate extends UIScript {
+		private mActionCon_Internal: mw.Canvas
+	public get mActionCon(): mw.Canvas {
+		if(!this.mActionCon_Internal&&this.uiWidgetBase) {
+			this.mActionCon_Internal = this.uiWidgetBase.findChildByPath('Canvas/mActionCon') as mw.Canvas
+		}
+		return this.mActionCon_Internal
+	}
+	private mContent3_Internal: mw.Canvas
+	public get mContent3(): mw.Canvas {
+		if(!this.mContent3_Internal&&this.uiWidgetBase) {
+			this.mContent3_Internal = this.uiWidgetBase.findChildByPath('Canvas/mActionCon/ScrollBox/mContent3') as mw.Canvas
+		}
+		return this.mContent3_Internal
+	}
+	private closeBtn3_Internal: mw.Button
+	public get closeBtn3(): mw.Button {
+		if(!this.closeBtn3_Internal&&this.uiWidgetBase) {
+			this.closeBtn3_Internal = this.uiWidgetBase.findChildByPath('Canvas/mActionCon/closeBtn3') as mw.Button
+		}
+		return this.closeBtn3_Internal
+	}
+
+
+ 
+	/**
+	* onStart 之前触发一次
+	*/
+	protected onAwake() {
+	}
+	 
+}
+ 
