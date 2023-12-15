cf9862ee20531861e5c118cd11783b4336c91cc7
Tomori6657 <japancjc123@gmail.com>
2023-12-15 11:44:17 +0800

    fix：修改土龙冰块逻辑

--- JavaScripts/gameplay/interactive/IceBlock.ts
+++ JavaScripts/gameplay/interactive/IceBlock.ts
@@ -2,7 +2,7 @@
  * @Author: 余泓 hong.yu@appshahe.com
  * @Date: 2023-12-11 15:42:26
  * @LastEditors: 余泓 hong.yu@appshahe.com
- * @LastEditTime: 2023-12-15 10:18:35
+ * @LastEditTime: 2023-12-15 11:07:42
  * @FilePath: \DragonVerse\JavaScripts\gameplay\interactive\IceBlock.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -61,7 +61,9 @@ export class IceBlock {
             this._trigger.worldTransform.rotation = arrayToRot(this._config.triggerRot);
             this._trigger.worldTransform.scale = arrayToVec(this._config.triggerScale);
             this._trigger.onEnter.add(this.onEnter);
-        })
+        });
+
+        AssetUtil.asyncDownloadAsset(IceBombGuid);
     }
 
     private onEnter = (obj: mw.GameObject) => {
--- JavaScripts/gameplay/quest/SoilDragonQuest.ts
+++ JavaScripts/gameplay/quest/SoilDragonQuest.ts
@@ -2,7 +2,7 @@
  * @Author: 余泓 hong.yu@appshahe.com
  * @Date: 2023-12-10 16:47:15
  * @LastEditors: 余泓 hong.yu@appshahe.com
- * @LastEditTime: 2023-12-14 17:40:08
+ * @LastEditTime: 2023-12-15 11:40:07
  * @FilePath: \DragonVerse\JavaScripts\gameplay\quest\SoilDragonQuest.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,6 +19,11 @@ import { Quest } from "./Quest";
  */
 @mw.Component
 export default class SoilDragonQuest extends Quest {
+
+    @mw.Property({ displayName: "全解锁后普通物块guid" })
+    private _objGuid: string = "";
+
+    private _obj: mw.GameObject;
 
     protected get progress(): number {
         return this.checkIsAllComplete();
@@ -56,14 +61,33 @@ export default class SoilDragonQuest extends Quest {
             const info = this._infos.find((val) => {
                 return val.index === index;
             });
+
+
+
+
             if (info) {
                 info.complete = true;
                 this.updateTaskProgress(JSON.stringify(this._infos));
             }
-
+            this.checkNormalIceBlock();
         });
 
+        this._obj = mw.GameObject.findGameObjectById(this._objGuid);
+        this.checkNormalIceBlock();
         this.creatIceBlock();
+
+
+
+
+
+    }
+
+
+    private checkNormalIceBlock() {
+        if (this._obj && this.checkIsAllComplete()) {
+            (this._obj as mw.Model).setCollision(mw.CollisionStatus.Off);
+            (this._obj as mw.Model).setVisibility(mw.PropertyStatus.Off);
+        }
     }
 
 
