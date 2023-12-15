421a4e2886bab0e164bb04e556dbdc2bcabe97cd
joji-mb <545633740@qq.com>
2023-12-15 15:09:52 +0800

    feat: add ChatBuff & MoveForbiddenBuff

--- JavaScripts/buffs/ChatBuff.ts
+++ JavaScripts/buffs/ChatBuff.ts
@@ -1,12 +1,11 @@
 import BuffBase from "../depend/buff/Buff";
 import UnifiedRoleController from "../module/role/UnifiedRoleController";
 import Log4Ts from "../depend/log4ts/Log4Ts";
-import HumanoidSlotType = mw.HumanoidSlotType;
 import EffectService = mw.EffectService;
 import { BuffType } from "./BuffType";
 
 /**
- * 湿 Buff.
+ * 交谈 Buff.
  * @desc ---
  * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
  * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
@@ -17,71 +16,33 @@ import { BuffType } from "./BuffType";
  * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
  * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
  */
-export class WetBuff extends BuffBase<UnifiedRoleController> {
+export class ChatBuff extends BuffBase<UnifiedRoleController> {
 //#region Constant
-    private static readonly WET_EFFECT_GUID = "113914";
-
-    private static readonly WET_EFFECT_SLOT = HumanoidSlotType.Buttocks;
-
-    private static get WET_EFFECT_POSITION() {
-        return new Vector(-4, 0, 15);
-    };
-
-    private static get WET_EFFECT_ROTATION() {
-        return new Rotation(4.11, 6.84, -7.49);
-    };
-
-    private static get WET_EFFECT_SCALE() {
-        return new Vector(1.1, 1.1, 1.1);
-    };
-
 //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
-    public type: BuffType = BuffType.Wet;
+    public type: BuffType = BuffType.Chat;
 
-    private _effectId: number;
+    public casterLocation: Vector;
 
     constructor(
-        target: UnifiedRoleController,
-        survivalStrategy: number) {
+        casterLocation: Vector,
+        target: UnifiedRoleController) {
         super(null,
             target,
-            survivalStrategy,
+            0,
             undefined,
             false);
+        this.casterLocation = casterLocation;
     }
 
     public onStart: () => void = () => {
-        this.replayEffect();
+        this.target.lookAtNpc(this.casterLocation);
+        this.target.addMoveForbiddenBuff();
         return;
     };
 
     public onRemove: () => void = () => {
-        EffectService.stop(this._effectId);
+        this.target.removeMoveForbiddenBuff();
         return;
     };
-
-    public onRefresh: () => void = () => {
-        this.replayEffect();
-    };
-
-    private replayEffect() {
-        const char = this.target.character;
-        if (!char) {
-            Log4Ts.error(WetBuff, `character in target is null`);
-            return;
-        }
-        EffectService.stop(this._effectId);
-        this._effectId = EffectService.playOnGameObject(
-            WetBuff.WET_EFFECT_GUID,
-            char,
-            {
-                duration: this.survivalStrategy / 1e3,
-                slotType: WetBuff.WET_EFFECT_SLOT,
-                position: WetBuff.WET_EFFECT_POSITION,
-                rotation: WetBuff.WET_EFFECT_ROTATION,
-                scale: WetBuff.WET_EFFECT_SCALE,
-            },
-        );
-    }
 }
--- JavaScripts/buffs/MoveForbiddenBuff.ts
+++ JavaScripts/buffs/MoveForbiddenBuff.ts
@@ -1,12 +1,11 @@
 import BuffBase from "../depend/buff/Buff";
 import UnifiedRoleController from "../module/role/UnifiedRoleController";
 import Log4Ts from "../depend/log4ts/Log4Ts";
-import HumanoidSlotType = mw.HumanoidSlotType;
 import EffectService = mw.EffectService;
 import { BuffType } from "./BuffType";
 
 /**
- * 湿 Buff.
+ * 移动禁止 Buff.
  * @desc ---
  * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
  * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
@@ -17,71 +16,28 @@ import { BuffType } from "./BuffType";
  * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
  * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
  */
-export class WetBuff extends BuffBase<UnifiedRoleController> {
+export class MoveForbiddenBuff extends BuffBase<UnifiedRoleController> {
 //#region Constant
-    private static readonly WET_EFFECT_GUID = "113914";
-
-    private static readonly WET_EFFECT_SLOT = HumanoidSlotType.Buttocks;
-
-    private static get WET_EFFECT_POSITION() {
-        return new Vector(-4, 0, 15);
-    };
-
-    private static get WET_EFFECT_ROTATION() {
-        return new Rotation(4.11, 6.84, -7.49);
-    };
-
-    private static get WET_EFFECT_SCALE() {
-        return new Vector(1.1, 1.1, 1.1);
-    };
-
 //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
-    public type: BuffType = BuffType.Wet;
-
-    private _effectId: number;
+    public type: BuffType = BuffType.MoveForbidden;
 
     constructor(
-        target: UnifiedRoleController,
-        survivalStrategy: number) {
+        target: UnifiedRoleController) {
         super(null,
             target,
-            survivalStrategy,
+            0,
             undefined,
             false);
     }
 
     public onStart: () => void = () => {
-        this.replayEffect();
+        this.target.character.movementEnabled = false;
         return;
     };
 
     public onRemove: () => void = () => {
-        EffectService.stop(this._effectId);
+        this.target.character.movementEnabled = true;
         return;
     };
-
-    public onRefresh: () => void = () => {
-        this.replayEffect();
-    };
-
-    private replayEffect() {
-        const char = this.target.character;
-        if (!char) {
-            Log4Ts.error(WetBuff, `character in target is null`);
-            return;
-        }
-        EffectService.stop(this._effectId);
-        this._effectId = EffectService.playOnGameObject(
-            WetBuff.WET_EFFECT_GUID,
-            char,
-            {
-                duration: this.survivalStrategy / 1e3,
-                slotType: WetBuff.WET_EFFECT_SLOT,
-                position: WetBuff.WET_EFFECT_POSITION,
-                rotation: WetBuff.WET_EFFECT_ROTATION,
-                scale: WetBuff.WET_EFFECT_SCALE,
-            },
-        );
-    }
 }
