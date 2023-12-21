f9f3c19f9088db1bd96a8f3f19b7679d0c077dc8
joji-mb <545633740@qq.com>
2023-12-21 15:00:56 +0800

    feat:开启特效循环

--- JavaScripts/gameplay/interactive/CircleTrigger.ts
+++ JavaScripts/gameplay/interactive/CircleTrigger.ts
@@ -6,7 +6,6 @@
  * @FilePath: \DragonVerse\JavaScripts\gameplay\interactive\CircleTrigger.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { getMutableClone } from "typescript";
 import { GameConfig } from "../../config/GameConfig";
 import { EventDefine } from "../../const/EventDefine";
 import { RunningGameGetParticle } from "../quest/runnungGame/RunningGameQuest";
