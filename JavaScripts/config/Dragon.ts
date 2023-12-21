17261f6f00be427cff6e205aab13f0a4920db48b
Tomori6657 <japancjc123@gmail.com>
2023-12-21 12:39:36 +0800

    更新配置表

--- JavaScripts/config/Dragon.ts
+++ JavaScripts/config/Dragon.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["id","avatar","elementalId","qualityId","areaIds","existenceTime","generationInterval","hitPoint","cost"],["","","","","","","","",""],[1,"0",1,1,[1,2,3,4,56,7,8,9,10,11,12],90,120,1,1],[2,"0",2,1,[1,2,3,4,56,7,8,9,10,11,12],90,120,1,1],[3,"0",3,1,[1,2,3,4,56,7,8,9,10,11,12],90,120,1,1],[4,"0",4,1,[1,2,3,4,56,7,8,9,10,11,12],90,120,1,1]];
+const EXCELDATA:Array<Array<any>> = [["id","avatar","elementalId","qualityId","areaIds","existenceTime","generationInterval","hitPoint","cost"],["","","","","","","","",""],[1,"0",1,1,[1,2,3,4,56,7,8,9,10,11,12],100,120,1,1],[2,"0",2,1,[1,2,3,4,56,7,8,9,10,11,12],100,120,1,1],[3,"0",3,1,[1,2,3,4,56,7,8,9,10,11,12],100,120,1,1],[4,"0",4,1,[1,2,3,4,56,7,8,9,10,11,12],100,120,1,1]];
 export interface IDragonElement extends IElementBase{
  	/**ID*/
 	id:number
