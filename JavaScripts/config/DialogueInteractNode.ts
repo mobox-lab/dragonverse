c7246d064e4dddf631168ef40b3ac2d26d9561cc
Tomori6657 <japancjc123@gmail.com>
2023-12-25 18:24:27 +0800

    feat: update config
    更新对话内容，
    更新NPC模型资源。

--- JavaScripts/config/DialogueInteractNode.ts
+++ JavaScripts/config/DialogueInteractNode.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["id","contentNodeId","content","funcId","icon"],["","","","",""],[1,3,"CharacterInteract0001",1,null],[2,0,"Exit",0,null],[3,7,"Dialogue0006",2,null],[4,8,"Dialogue0007",0,null],[5,0,"CharacterInteract0002",5,null]];
+const EXCELDATA:Array<Array<any>> = [["id","contentNodeId","content","funcId","icon"],["","","","",""],[1,3,"CharacterInteract0001",1,null],[2,0,"Exit",0,null],[3,7,"Dialogue0006",2,null],[4,8,"Dialogue0007",0,null],[5,0,"CharacterInteract0002",5,null],[6,11,"CharacterInteract0001",1,null],[7,27,"CharacterInteract0001",1,null],[8,43,"CharacterInteract0001",1,null],[9,50,"CharacterInteract0001",1,null],[10,59,"CharacterInteract0001",1,null],[11,68,"CharacterInteract0001",1,null],[12,75,"CharacterInteract0001",1,null]];
 export interface IDialogueInteractNodeElement extends IElementBase{
  	/**ID*/
 	id:number
