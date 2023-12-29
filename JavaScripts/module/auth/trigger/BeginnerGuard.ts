7240c390cb7b4788f3d9c121255b98a34e564fa6
joji-mb <545633740@qq.com>
2023-12-29 16:59:42 +0800

    feat: update BeginnerGuard

--- JavaScripts/module/auth/trigger/BeginnerGuard.ts
+++ JavaScripts/module/auth/trigger/BeginnerGuard.ts
@@ -9,7 +9,7 @@ import MainPanel from "../../../ui/main/MainPanel";
 import { AuthModuleC } from "../AuthModule";
 
 /**
- * 采集物 Trigger.
+ * 新手村 守卫.
  *
  * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
  * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
@@ -20,41 +20,10 @@ import { AuthModuleC } from "../AuthModule";
  */
 @Component
 export default class BeginnerGuard extends mw.Script {
-    //#region Constant
-    /**
-     * 守卫 Npc Id.
-     * @private
-     */
-    @mw.Property({ displayName: "守卫 Npc Id" })
-    private guardNpcId: number = 0;
-
-    // /**
-    //  * 禁行区 Guid.
-    //  * @private
-    //  */
-    // @mw.Property({ displayName: "禁行区 Guid" })
-    // private forbiddenAreaGuid: string = "";
-
-    /**
-     * 守卫警告对话内容节点 Id.
-     * @private
-     */
-    @mw.Property({ displayName: "守卫警告对话内容节点 Id" })
-    private guardContentId: number = 1;
-
-    //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
-
     //#region Member
-    private _guardNpc: NpcBehavior = null;
-
     private _forbiddenArea: BlockingVolume = null;
 
     private _blockEffect: Effect = null;
-
-    private get guardNpc() {
-        if (!this._guardNpc) this._guardNpc = ModuleService.getModule(NpcModuleC)?.getNpc(this.guardNpcId);
-        return this._guardNpc;
-    }
 
     //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
@@ -77,18 +46,7 @@ export default class BeginnerGuard extends mw.Script {
         //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
         //region Event subscribe
-        let gameObject: mw.GameObject = this.gameObject;
-        if (gameObject) {
-            let trigger = gameObject.getChildByName("trigger") as mw.Trigger;
-            if (trigger) {
-                trigger.onEnter.add(this.onObjectEnter);
-            } else {
-                Log4Ts.error(BeginnerGuard, `there is no trigger under this game object.`);
-            }
-        }
-
         this._blockEffect = this.gameObject.getChildByName("blockEffect") as Effect;
-
 
         Event.addLocalListener(EventDefine.PlayerEnableEnter, this.onPlayerEnableEnter);
         Event.addLocalListener(EventDefine.PlayerDisableEnter, this.onPlayerDisableEnter.bind(this));
@@ -106,26 +64,6 @@ export default class BeginnerGuard extends mw.Script {
     //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
     //region Event Callback
-    private onObjectEnter = (obj: GameObject): void => {
-        if (GToolkit.isSelfCharacter(obj)) {
-            const playerId = (obj as Character).player.playerId;
-            this.characterEnter(playerId);
-        }
-    };
-
-
-    private characterEnter = (playerId: number) => {
-        Log4Ts.log(BeginnerGuard, `player enter. playerId: ${playerId}`);
-        if (playerId !== Player.localPlayer.playerId) return;
-
-        if (this._forbiddenArea.getTargetPassable(Player.localPlayer.character)) return;
-
-        if (this.guardContentId === 0) {
-            Log4Ts.error(BeginnerGuard, `guard content node not found. Id: ${this.guardContentId}`);
-            return;
-        }
-        DialogueManager.getInstance().chat(this.guardContentId, true);
-    };
 
     private onPlayerEnableEnter = () => {
         this._blockEffect.setColor("Color", LinearColor.colorToLinearColor(101, 191, 214));
