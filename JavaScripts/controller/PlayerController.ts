7b017aa4e59cb5b5d264474360e3b051bdb80141
Tomori6657 <japancjc123@gmail.com>
2024-01-10 16:41:39 +0800

    feat：增加游泳时跳跃

--- JavaScripts/controller/PlayerController.ts
+++ JavaScripts/controller/PlayerController.ts
@@ -1,21 +1,61 @@
+import Log4Ts, { DebugLevels } from "../depend/log4ts/Log4Ts";
 import { Singleton } from "../depend/singleton/Singleton";
 import { KeyboardManager } from "./KeyboardManager";
 
 export class PlayerController extends Singleton<PlayerController>() {
 
-
+    private _isSwimingInArea1 = false;
+    private _isSwimingInArea2 = false;
     private _velocity: mw.Vector = mw.Vector.zero;
 
-
     protected onConstruct(): void {
+        Log4Ts.debugLevel = DebugLevels.Silent;
+        let swimArea1 = GameObject.findGameObjectById("0431AECB") as SwimmingVolume;
+        let swimArea2 = GameObject.findGameObjectById("2A9D1832") as SwimmingVolume;
+        swimArea1.onEnter.add((go) => {
+            Player.asyncGetLocalPlayer().then((player) => {
+                if (go instanceof Character && go === player.character) {
+                    this._isSwimingInArea1 = true;
+                }
+            })
+        });
+        swimArea2.onEnter.add((go) => {
+            Player.asyncGetLocalPlayer().then((player) => {
+                if (go instanceof Character && go === player.character) {
+                    this._isSwimingInArea2 = true;
+                }
+            })
+        });
+        swimArea1.onLeave.add((go) => {
+            Player.asyncGetLocalPlayer().then((player) => {
+                if (go instanceof Character && go === player.character) {
+                    this._isSwimingInArea1 = false;
+                }
+            })
+        });
+        swimArea2.onLeave.add((go) => {
+            Player.asyncGetLocalPlayer().then((player) => {
+                if (go instanceof Character && go === player.character) {
+                    this._isSwimingInArea2 = false;
+                }
+            })
+        });
+
 
         KeyboardManager.getInstance().onKeyDown.add((key) => {
 
             if (key === mw.Keys.SpaceBar) {
-
-                mw.Player.localPlayer.character.jump();
+                if (!this._isSwimingInArea1 && !this._isSwimingInArea2) {
+                    mw.Player.localPlayer.character.jump();
+                } else {
+                    actions.tween(Player.localPlayer.character.worldTransform).to(10,
+                        { position: Player.localPlayer.character.worldTransform.position.clone().add(new Vector(0, 0, 100)) },).call(() => {
+                            Player.localPlayer.character.jump();
+                        }).start();
+                }
             }
         })
+
         TimeUtil.onEnterFrame.add(this.onEnterFrame, this)
     }
 
