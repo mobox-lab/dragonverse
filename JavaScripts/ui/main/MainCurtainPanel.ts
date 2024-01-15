45aa50824e2f4dd6556662522c65e25231216da4
joji-mb <545633740@qq.com>
2024-01-15 16:29:58 +0800

    feat: add MCP

---
+++ JavaScripts/ui/main/MainCurtainPanel.ts
@@ -0,0 +1,75 @@
+import MainCurtainPanel_Generate from "../../ui-generate/main/MainCurtainPanel_generate";
+import Waterween from "../../depend/waterween/Waterween";
+import GameServiceConfig from "../../const/GameServiceConfig";
+import { FlowTweenTask } from "../../depend/waterween/tweenTask/FlowTweenTask";
+import Easing, { CubicBezierBase } from "../../depend/easing/Easing";
+import Log4Ts from "../../depend/log4ts/Log4Ts";
+
+export default class MainCurtainPanel extends MainCurtainPanel_Generate {
+//#region Constant
+    public static readonly MAIN_SHOW_CURTAIN_EVENT_NAME = "__MAIN_SHOW_CURTAIN__";
+    public static readonly MAIN_HIDE_CURTAIN_EVENT_NAME = "__MAIN_HIDE_CURTAIN__";
+//#endregion
+
+//#region Member
+    private _curtainTask: FlowTweenTask<number>;
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region MetaWorld UI Event
+
+    protected onAwake(): void {
+        super.onAwake();
+        this.canUpdate = true;
+
+//#region Member init
+        this._curtainTask = Waterween
+            .flow(
+                () => this.cnvCurtain.renderOpacity,
+                (val) => {
+                    Log4Ts.log(MainCurtainPanel, `shown opa update val: ${val}`);
+                    this.cnvCurtain.renderOpacity = val;
+                },
+                GameServiceConfig.MAIN_PANEL_CURTAIN_SHOWN_DURATION,
+                Easing.linear,
+                undefined,
+                true,
+                false,
+            );
+
+//#endregion ------------------------------------------------------------------------------------------
+
+//#region Widget bind
+        Event.addLocalListener(MainCurtainPanel.MAIN_SHOW_CURTAIN_EVENT_NAME, () => this.showCurtain(true));
+        Event.addLocalListener(MainCurtainPanel.MAIN_HIDE_CURTAIN_EVENT_NAME, () => this.showCurtain(false));
+//#endregion ------------------------------------------------------------------------------------------
+
+//#region Event subscribe
+//#endregion ------------------------------------------------------------------------------------------
+    }
+
+    protected onUpdate() {
+    }
+
+    protected onShow() {
+    }
+
+    protected onHide() {
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Init
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region UI Behavior
+    public showCurtain(enable: boolean = true) {
+        if (enable) {
+            this._curtainTask.to(1, GameServiceConfig.MAIN_PANEL_CURTAIN_SHOWN_DURATION);
+        } else {
+            this._curtainTask.to(0, GameServiceConfig.MAIN_PANEL_CURTAIN_HIDDEN_DURATION);
+            Log4Ts.log(MainCurtainPanel, `curtain hide at ${Date.now()}`);
+        }
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
