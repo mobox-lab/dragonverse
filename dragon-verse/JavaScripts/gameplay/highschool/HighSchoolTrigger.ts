d23f0cb30714bf0f722887dcdb0d8372ff1cccc4
joji-mb <545633740@qq.com>
2024-01-24 16:00:12 +0800

    添加trigger逻辑

--- dragon-verse/JavaScripts/gameplay/highschool/HighSchoolTrigger.ts
+++ dragon-verse/JavaScripts/gameplay/highschool/HighSchoolTrigger.ts
@@ -18,6 +18,9 @@ import { RunningGameGetParticle } from "../quest/runnungGame/RunningGameQuest";
 export enum HighSchoolType {
     /**起点 */
     TransStart = 1,
+    DeadBackGround = 2,//地下的死亡触发点，碰到后就锁定视角，判定死亡
+    DeadRed = 3,//红色方块 触碰后变成布娃娃，然后回到原点
+    ScorePoint = 4,//得分点 碰到后就得分
 }
 
 /**
@@ -34,9 +37,25 @@ export default class HighSchoolTrigger extends mw.Script {
     })
     private _circleType: HighSchoolType = HighSchoolType.TransStart;
 
+    @mw.Property({ displayName: "跑酷最大速度" })
+    public walkMaxSpeed: number = 1;
+    @mw.Property({ displayName: "跑酷最大跳跃次数" })
+    public jumpMaxCount: number = 1;
+    @mw.Property({ displayName: "不可跨越高度" })
+    public jumpMaxHeight: number = 1;
+
     private _trigger: mw.Trigger;
 
-    private _obj: mw.GameObject;
+    private maxWalkSpeedOld = 0; //地面最大速度
+    private maxAccelerationOld = 0;//地面最大加速度
+    // private maxJumpHeight = 0; //不可跨越高度
+    // private maxJumpHeight = 0; //最大站稳角度
+    private rotateRateOld = 0; //最大转向速度
+    // private maxJumpHeightOld = 0; //地面摩擦力
+    // private maxJumpHeight = 0; //下落速度
+    // private maxJumpHeight = 0; //重力倍率
+    private maxJumpHeightOld = 0; //最大跳跃高度
+    private jumpMaxCountOld = 0; //最大跳跃次数
 
     protected onStart(): void {
         if (mw.SystemUtil.isServer()) {
@@ -54,21 +73,36 @@ export default class HighSchoolTrigger extends mw.Script {
     }
 
     private onEnter = (obj: mw.GameObject) => {
-        console.log("onEnter iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
         if (obj instanceof mw.Character) {
             if (GToolkit.isSelfCharacter(obj)) {
-
                 if (this._circleType == HighSchoolType.TransStart) {
-                    console.log("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
-                    obj.maxJumpHeight = 100;
-                    obj.jumpMaxCount = 1;
-                    obj.maxWalkSpeed = 100;
+                    this.recordOldValue(obj);
+                    obj.maxJumpHeight = this.jumpMaxHeight;
+                    obj.jumpMaxCount = this.jumpMaxCount;
+                    obj.maxWalkSpeed = this.walkMaxSpeed;
+                    
                     UIService.getUI(MainPanel).setCanSprint(false);
+                }else if (this._circleType == HighSchoolType.DeadBackGround) {
+                    
+                }else if (this._circleType == HighSchoolType.DeadRed) {
+
                 }
 
             }
         }
     };
+
+    private recordOldValue(obj:Character){
+        this.maxWalkSpeedOld = obj.maxWalkSpeed;
+        this.maxAccelerationOld = obj.maxAcceleration;
+        this.rotateRateOld = obj.rotateRate;
+        this.maxJumpHeightOld = obj.maxJumpHeight;
+        this.jumpMaxCountOld = obj.jumpMaxCount;
+    }
+
+    private reborn(){
+
+    }
 
 
 }
