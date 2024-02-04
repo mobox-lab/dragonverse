d2a436d3c75252518dbbea919df656b05f111bb2
joji-mb <545633740@qq.com>
2024-02-04 17:37:21 +0800

    feat: update PBMD

--- pet-simulator/JavaScripts/const/GlobalData.ts
+++ pet-simulator/JavaScripts/const/GlobalData.ts
@@ -1256,7 +1256,7 @@ export namespace GlobalData {
          * 体力 恢复间隔. min
          * @type {number}
          */
-        public static readonly ENERGY_RECOVERY_INTERVAL: number = 30;
+        public static readonly ENERGY_RECOVERY_INTERVAL: number = 5;
 
         /**
          * 体力 恢复间隔. ms
@@ -1282,7 +1282,7 @@ export namespace GlobalData {
          * 体力 倍率.
          * @type {number}
          */
-        public static readonly ENERGY_RATIO = 2;
+        public static readonly ENERGY_RATIO = 1/3;
     }
 
     /**
