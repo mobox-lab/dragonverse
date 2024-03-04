bbc0a8e4a546fbfe354ff3f382e21f7141ff3263
joji-mb <545633740@qq.com>
2024-03-04 18:46:37 +0800

    feat: add ObbyStar

--- dragon-verse/JavaScripts/gameplay/obby/ObbyStar.ts
+++ dragon-verse/JavaScripts/gameplay/obby/ObbyStar.ts
@@ -1,25 +1,12 @@
 import Log4Ts from "../../depend/log4ts/Log4Ts";
-import GToolkit from "../../util/GToolkit";
+import Gtk from "../../util/GToolkit";
 import GameObject = mw.GameObject;
-import { EventDefine } from "../../const/EventDefine";
-
-export enum FirePuzzleBlockTypes {
-    /**
-     * 空置.
-     */
-    Null,
-    /**
-     * 岩浆.
-     */
-    Magma,
-    /**
-     * 水.
-     */
-    Water,
-}
+import {EventDefine} from "../../const/EventDefine";
+import GToolkit from "../../util/GToolkit";
+import GameServiceConfig from "../../const/GameServiceConfig";
 
 /**
- * 火炎地狱 谜题方块 Behavior.
+ * DragonVerse Obby Star Behavior.
  *
  * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
  * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
@@ -31,7 +18,7 @@ export enum FirePuzzleBlockTypes {
  * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
  */
 @Component
-export default class FirePuzzleBlock extends mw.Script {
+export default class ObbyStar extends mw.Script {
 //#region Constant
     private static readonly BLOCK_MAGMA_MATERIAL_ASSET_ID = "6EAB7AFB42853BE6F1B1989C46D3ED77";
 
@@ -39,14 +26,11 @@ export default class FirePuzzleBlock extends mw.Script {
 
     private static readonly GAME_OBJECT_MESH_NAME = "mesh";
 
-    private static readonly GAME_OBJECT_TRIGGER_NAME = "trigger";
+    private static readonly GAME_OBJECT_TRIGGER_NAME = "obbyStarTrigger";
 
 //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
 //#region Member
-
-    @Property({displayName: "方块类型", group: "Config | block", enumType: FirePuzzleBlockTypes})
-    public originBlockType: FirePuzzleBlockTypes = FirePuzzleBlockTypes.Water;
 
     @Property({displayName: "湿润 Buff 持续时长 s", group: "Config | buff"})
     public wetBuffDuration: number = 5;
@@ -54,38 +38,55 @@ export default class FirePuzzleBlock extends mw.Script {
     @Property({displayName: "爆炸冲量力", group: "Config | buff"})
     public force: number = 1000;
 
-    private _clientBlockType: FirePuzzleBlockTypes = FirePuzzleBlockTypes.Null;
-
-    private _meshObj: Model;
-
     private _trigger: Trigger;
 
+    private _flying: boolean;
+
+    private _flySpeed: number = 0;
+
+    private _clientCharacter: mw.Character;
+
+    private _serverUsedMap: Map<number, boolean> = new Map();
 
 //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
 //region MetaWorld Event
     protected onStart(): void {
         super.onStart();
+        this.useUpdate = true;
 
 //region Member init
-        if (SystemUtil.isClient()) {
-            this._meshObj = GToolkit.getFirstGameObject(this.gameObject, FirePuzzleBlock.GAME_OBJECT_MESH_NAME) as Model;
-            this.switchType(this.originBlockType);
-            if (!this._meshObj) {
-                Log4Ts.error(FirePuzzleBlock, `client there is no mesh object in this block.`);
-                return;
-            }
-        } else if (SystemUtil.isServer()) {
-            this._trigger = GToolkit.getFirstGameObject(this.gameObject, FirePuzzleBlock.GAME_OBJECT_TRIGGER_NAME) as Trigger;
+
+        if (SystemUtil.isServer()) {
+            this._trigger = Gtk.getFirstGameObject(this.gameObject, ObbyStar.GAME_OBJECT_TRIGGER_NAME) as Trigger;
             if (!this._trigger) {
-                Log4Ts.error(FirePuzzleBlock, `there is no trigger under this game object.`);
+                Log4Ts.error(ObbyStar, `there is no trigger under this game object.`);
             } else {
                 this._trigger.onEnter.add(this.onObjectEnter);
                 this._trigger.onLeave.add(this.onObjectLeave);
             }
+        } else if (SystemUtil.isClient()) {
+            this._clientCharacter = Player.localPlayer.character;
         }
     }
 
+    protected onUpdate(dt: number): void {
+        super.onUpdate(dt);
+
+        if (SystemUtil.isClient() && this._flying) {
+            const direction = this.gameObject.worldTransform.position.subtract(this._clientCharacter.worldTransform.position).normalize();
+            this._flySpeed = Math.min(this._flySpeed + GameServiceConfig.OBBY_STAR_FLY_ACCELERATED * dt, GameServiceConfig.OBBY_STAR_FLY_MAX_SPEED);
+            this.gameObject.worldTransform.position = this.gameObject.worldTransform.position.add(direction.multiply(this._flySpeed * dt));
+
+
+            if (this.gameObject.worldTransform.position.squaredDistanceTo(this._clientCharacter.worldTransform.position) < 1) {
+                this._flying = false;
+                this._flySpeed = 0;
+                this.gameObject.setVisibility(false);
+                this.reportFlyFinished(this._clientCharacter.player.playerId);
+            }
+        }
+    }
 
 //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
@@ -103,37 +104,38 @@ export default class FirePuzzleBlock extends mw.Script {
 
 //#region Method
     @RemoteFunction(Client)
-    public switchType(type: FirePuzzleBlockTypes) {
-        if (this._clientBlockType === type) return;
+    public flyToPlayer(player: mw.Player) {
+        this._flying = true;
+    }
 
-        let materialAssetId: string;
-        switch (type) {
-            case FirePuzzleBlockTypes.Magma:
-                materialAssetId = FirePuzzleBlock.BLOCK_MAGMA_MATERIAL_ASSET_ID;
-                break;
-            case FirePuzzleBlockTypes.Water:
-                materialAssetId = FirePuzzleBlock.BLOCK_WATER_MATERIAL_ASSET_ID;
-                break;
-            default:
-                Log4Ts.error(FirePuzzleBlock, `client switch type error. type: ${type} is not supported.`);
-                break;
+    @RemoteFunction(Server)
+    public reportFlyFinished(playerId: number) {
+        if (this._serverUsedMap.get(playerId) === false) {
+            this._serverUsedMap.set(playerId, true);
+//TODO_LviatYi Add Star
         }
-        this._meshObj.setMaterial(materialAssetId);
-        Log4Ts.log(FirePuzzleBlock, `client change block type success. current type: ${type}`);
+    }
+
+    @RemoteFunction(Client)
+    public clientReset(player: mw.Player) {
+        this._flying = false;
+        this._flySpeed = 0;
+        this.gameObject.setVisibility(true);
+    }
+
+    public serverReset(player: mw.Player) {
+        this.clientReset(player);
+        this._serverUsedMap.delete(player.playerId);
     }
 
 //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 
 //region Event Callback
     private onObjectEnter = (obj: GameObject): void => {
-        if (GToolkit.isCharacter(obj)) {
-            Event.dispatchToClient(
-                obj.player,
-                EventDefine.PlayerEnterFirePuzzleBlock,
-                this.gameObject.gameObjectId,
-                this.force,
-                this.wetBuffDuration,
-            );
+        if (Gtk.isCharacter(obj)) {
+            const player = obj.player;
+            this.flyToPlayer(player);
+            this._serverUsedMap.set(player.playerId, false);
         }
     };
 
@@ -146,7 +148,6 @@ export default class FirePuzzleBlock extends mw.Script {
                 this.wetBuffDuration);
         }
     };
-
 
 //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 }
