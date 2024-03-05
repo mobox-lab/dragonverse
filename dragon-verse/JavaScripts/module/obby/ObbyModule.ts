2677668b0862e1e6a272abd3a62c0033b5f140aa
Tomori6657 <japancjc123@gmail.com>
2024-03-05 18:31:28 +0800

    feat: add comsume coin

--- dragon-verse/JavaScripts/module/obby/ObbyModule.ts
+++ dragon-verse/JavaScripts/module/obby/ObbyModule.ts
@@ -1,29 +1,29 @@
 import Enumerable from "linq";
 import UUID from "pure-uuid";
-import {GameConfig} from "../../config/GameConfig";
-import {EventDefine} from "../../const/EventDefine";
-import {BagTypes} from "../../const/ForeignKeyIndexer";
+import { GameConfig } from "../../config/GameConfig";
+import { EventDefine } from "../../const/EventDefine";
+import { BagTypes } from "../../const/ForeignKeyIndexer";
 import GameServiceConfig from "../../const/GameServiceConfig";
 import Log4Ts from "../../depend/log4ts/Log4Ts";
 import Regulator from "../../depend/regulator/Regulator";
 import AreaManager from "../../gameplay/area/AreaManager";
 import MainPanel from "../../ui/main/MainPanel";
 import GToolkit from "../../util/GToolkit";
-import {IPoint3} from "../../util/area/Shape";
-import {BagModuleS} from "../bag/BagModule";
+import { IPoint3 } from "../../util/area/Shape";
+import { BagModuleS } from "../bag/BagModule";
 import noReply = mwext.Decorator.noReply;
 import GameObject = mw.GameObject;
 import GameObjPoolSourceType = mwext.GameObjPoolSourceType;
 import EventListener = mw.EventListener;
 import Enum = UE.Enum;
-import {ObbyInteractorPanel} from "../../ui/obby/ObbyInteractorPanel";
-import {DataUpgradeMethod} from "../../depend/jibu-module/JModule";
+import { ObbyInteractorPanel } from "../../ui/obby/ObbyInteractorPanel";
+import { DataUpgradeMethod } from "../../depend/jibu-module/JModule";
 import Nolan from "../../depend/nolan/Nolan";
 import i18n from "../../language/i18n";
 import UnifiedRoleController from "../role/UnifiedRoleController";
-import {GameEndPanel} from "../../ui/obby/GameEndPanel";
-import {MapManager} from "../../gameplay/map/MapManager";
-import {KeyboardManager} from "../../controller/KeyboardManager";
+import { GameEndPanel } from "../../ui/obby/GameEndPanel";
+import { MapManager } from "../../gameplay/map/MapManager";
+import { KeyboardManager } from "../../controller/KeyboardManager";
 import GTkTypes from "../../util/GToolkit";
 
 
@@ -625,13 +625,18 @@ export class ObbyModuleS extends ModuleS<ObbyModuleC, ObbyModuleData> {
             return Promise.resolve(false);
         } else {
             //判断有没钱
-            this._playerIsInvincible.set(this.currentPlayerId, true);
-            let playerId = this.currentPlayerId;
-            this.getClient(this.currentPlayerId).net_setInvincibleSuccess();
+            let res = ModuleService.getModule(BagModuleS).consumeObbyCoin(this.currentPlayerId);
+            if (res) {
+                this._playerIsInvincible.set(this.currentPlayerId, true);
+                let playerId = this.currentPlayerId;
+                this.getClient(this.currentPlayerId).net_setInvincibleSuccess();
 
-            await mw.TimeUtil.delaySecond(10);
-            this._playerIsInvincible.set(playerId, false);
-            return Promise.resolve(true);
+                await mw.TimeUtil.delaySecond(10);
+                this._playerIsInvincible.set(playerId, false);
+                return Promise.resolve(true);
+            } else {
+                return Promise.resolve(false);
+            }
         }
     }
 
@@ -645,7 +650,8 @@ export class ObbyModuleS extends ModuleS<ObbyModuleC, ObbyModuleData> {
 
     public async net_autoFindPath(): Promise<boolean> {
         if (this._playerIsAutoMove.get(this.currentPlayerId) !== true) {
-            //不是无敌状态再扣钱
+            //不是再扣钱
+
             let character = Player.getPlayer(this.currentPlayerId).character;
             if (!character) return;
 
@@ -660,7 +666,7 @@ export class ObbyModuleS extends ModuleS<ObbyModuleC, ObbyModuleData> {
             this._playerIsAutoMove.set(this.currentPlayerId, true);
             let playerId = this.currentPlayerId;
 
-            actions.tween(character.worldTransform).to(1000, {position: pos}).call(() => {
+            actions.tween(character.worldTransform).to(1000, { position: pos }).call(() => {
                 animation.stop();
                 character.gravityScale = oriGravity;
                 character.movementEnabled = true;
---
+++ dragon-verse/JavaScripts/ui/obby/ObbyEnterFailPanel.ts
@@ -0,0 +1,18 @@
+/** 
+ * @Author       : zewei.zhang
+ * @Date         : 2024-03-05 18:09:52
+ * @LastEditors  : zewei.zhang
+ * @LastEditTime : 2024-03-05 18:27:43
+ * @FilePath     : \DragonVerse\dragon-verse\JavaScripts\ui\obby\ObbyEnterFailPanel.ts
+ * @Description  : 修改描述
+ */
+
+import doubleconfirm_Generate from "../../ui-generate/subgame/doubleconfirm_generate";
+
+export default class ObbyEnterPanel extends doubleconfirm_Generate {
+    onStart() {
+        this.codeButtonClose.onClicked.add(() => {
+
+        })
+    }
+}
