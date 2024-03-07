835c97d4640815be80b2787c60a2e7488f64b144
Tomori6657 <japancjc123@gmail.com>
2024-03-07 17:45:19 +0800

    fix：portaltrigger no extends

--- dragon-verse/JavaScripts/gameplay/obby/ObbyPortalTrigger.ts
+++ dragon-verse/JavaScripts/gameplay/obby/ObbyPortalTrigger.ts
@@ -7,15 +7,127 @@
  * @Description  : 修改描述
  */
 
+import Log4Ts from "../../depend/log4ts/Log4Ts";
+import Nolan from "../../depend/nolan/Nolan";
 import { BagModuleC } from "../../module/bag/BagModule";
 import ObbyEnterFailPanel from "../../ui/obby/ObbyEnterFailPanel";
 import ObbyEnterPanel from "../../ui/obby/ObbyEnterPanel";
+import Gtk from "../../util/GToolkit";
+import GToolkit from "../../util/GToolkit";
 
 import PortalTrigger from "../portal/PortalTrigger";
+@Component
+export default class ObbyPortalTrigger extends Script {
+    //#region Member
 
-export default class ObbyPortalTrigger extends PortalTrigger {
+    @Property({ displayName: "传送目的地", group: "Config-location" })
+    public destination: Vector = Vector.zero;
 
-    protected async transferPlayer(character: mw.Character) {
+    @Property({ displayName: "传送目标", group: "Config-location", tooltip: "传送目标的 guid. 优先生效." })
+    public destinationTargetGuid: string = "";
+
+    @Property({
+        displayName: "是否刷新玩家物体旋转",
+        tooltip: "true 时 将玩家物体旋转调整为 endRotation.",
+        group: "Config-rotation",
+    })
+    public isRefreshObjectRotation: boolean = true;
+
+    @Property({
+        displayName: "是否刷新相机旋转",
+        tooltip: "true 时 将玩家相机调整为 endRotation.",
+        group: "Config-rotation",
+    })
+    public isRefreshCameraRotation: boolean = true;
+
+    @Property({ displayName: "角色目标旋转", group: "Config-rotation" })
+    public endRotation: Rotation = Rotation.zero;
+
+    @Property({
+        displayName: "是否保持原有速度",
+        tooltip: "若目的地过于贴近地面 原有速度将受摩擦力影响\n旋转将跟随 isRefreshObjectRotation",
+        group: "Config-speed",
+    })
+    public isHoldVelocity: boolean = false;
+
+    private _nolan: Nolan;
+
+    //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    //region MetaWorld Event
+    protected onStart(): void {
+        super.onStart();
+
+        if (mw.SystemUtil.isServer()) {
+            return;
+        }
+        //region Member init
+        this._nolan = Nolan.getInstance();
+        //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+        //region Widget bind
+        //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+        //region Event subscribe
+        let gameObject: mw.GameObject = this.gameObject;
+        if (gameObject) {
+            let trigger = gameObject as mw.Trigger;
+            if (trigger) {
+                trigger.onEnter.add(this.onObjectEnter);
+            } else {
+                Log4Ts.error(PortalTrigger, `there is no trigger under this game object.`);
+            }
+        }
+        //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+    }
+
+    protected onDestroy(): void {
+        super.onDestroy();
+    }
+
+    //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    //#region Method
+    //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    //region Event Callback
+    private onObjectEnter = (obj: GameObject): void => {
+        if (GToolkit.isSelfCharacter(obj)) {
+            this.onEnter(obj as Character);
+        }
+    };
+
+    protected transferPlayer(character: Character) {
+        Log4Ts.log(PortalTrigger, `player enter. playerId: ${character?.player?.playerId ?? "null"}`);
+
+        if (character) {
+            const player = character.player;
+            if (player) {
+                const velocity = character.velocity;
+
+                character.addImpulse(velocity.clone().multiply(Vector.negOne), true);
+                character.worldTransform = new Transform(
+                    Gtk.isNullOrEmpty(this.destinationTargetGuid) ?
+                        this.destination :
+                        GameObject.findGameObjectById(this.destinationTargetGuid)?.worldTransform.position ?? this.destination,
+                    this.isRefreshObjectRotation ? this.endRotation : character.worldTransform.rotation,
+                    character.worldTransform.scale,
+                );
+
+                if (this.isRefreshCameraRotation) this._nolan.lookToward(this.endRotation.rotateVector(Vector.forward));
+
+                if (this.isHoldVelocity) {
+                    let v = this.isRefreshObjectRotation ?
+                        this.endRotation.rotateVector(Vector.forward).multiply(velocity.length) :
+                        velocity;
+
+                    character.addImpulse(v, true);
+                }
+            }
+        }
+    }
+
+    protected async onEnter(character: mw.Character) {
         //先弹确认ui
         //先本地判断下够不够次数
         if (UIService.getUI(ObbyEnterPanel).isShowing || UIService.getUI(ObbyEnterFailPanel).isShowing) return;
@@ -26,7 +138,7 @@ export default class ObbyPortalTrigger extends PortalTrigger {
                 //检查次数
                 let res = await ModuleService.getModule(BagModuleC).consumeObbyTicket();
                 if (res) {
-                    super.transferPlayer(character);
+                    this.transferPlayer(character);
                 } else {
                     UIService.show(ObbyEnterFailPanel);
                 }
