8413c54b3d905ffd6699c73d9ad6c572a5870288
joji-mb <545633740@qq.com>
2024-03-24 18:38:54 +0800

    feat: import ScrollView30.2.1b

--- hauntedparadise/JavaScripts/depend/scroll-view/ScrollView.ts
+++ hauntedparadise/JavaScripts/depend/scroll-view/ScrollView.ts
@@ -59,7 +59,7 @@ import Canvas = mw.Canvas;
  * @author zewei.zhang
  * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
  * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
- * @version 1.0.7b
+ * @version 30.2.1b
  */
 export default class ScrollView<
     D extends IUnique,
@@ -144,6 +144,8 @@ export default class ScrollView<
                         true,
                         true,
                     );
+                    container.autoSizeVerticalEnable = false;
+                    container.autoSizeHorizontalEnable = true;
                     break;
                 case mw.Orientation.OrientVertical:
                     if (padding.right > 0) {
@@ -164,6 +166,8 @@ export default class ScrollView<
                         true,
                         true,
                     );
+                    container.autoSizeHorizontalEnable = false;
+                    container.autoSizeVerticalEnable = true;
                     break;
             }
         }
