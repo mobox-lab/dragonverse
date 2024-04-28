a614b5c028b8dc6fa5499a761674c371a2339b5f
Tomori6657 <japancjc123@gmail.com>
2024-04-28 13:11:31 +0800

    feat：对话选项ui

---
+++ dragon-verse/JavaScripts/ui/dialogue/InteractionNode.ts
@@ -0,0 +1,21 @@
+import GameServiceConfig from "../../const/GameServiceConfig";
+import KeyOperationManager from "../../controller/key-operation-manager/KeyOperationManager";
+import InteractNode_Generate from "../../ui-generate/dialogue/InteractNode_generate";
+
+export default class InteractionNode extends InteractNode_Generate {
+    onStart(): void {
+        super.onStart();
+        KeyOperationManager.getInstance().onWidgetEntered(this.uiObject, () => {
+            this.btnMain.normalImageGuid = GameServiceConfig.DIALOGUE_PANEL_OPTION_ON_HOVER_IMG_GUID;
+        });
+
+        KeyOperationManager.getInstance().onWidgetLeave(this.uiObject, () => {
+            this.btnMain.normalImageGuid = GameServiceConfig.DIALOGUE_PANEL_OPTION_NORMAL_IMG_GUID;
+        });
+    }
+
+    onDestroy(): void {
+        KeyOperationManager.getInstance().unregisterMouse(this.uiObject);
+        console.log("destroy");
+    }
+}
