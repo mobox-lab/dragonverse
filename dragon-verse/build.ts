3f24b36c2c0b3433130ae0382b68026fc4629f71
Tomori6657 <japancjc123@gmail.com>
2024-05-08 11:13:28 +0800

    feat：去掉animation依赖防止偶现编译问题，环境表更新，头顶ui销毁时结束自适应轮询

--- dragon-verse/build.ts
+++
@@ -1,9 +0,0 @@
-import "./JavaScripts/lighter/depends/core";
-
-import "./JavaScripts/lighter/depends/animation";
-
-
-import "./JavaScripts/lighter/core";
-
-import "./JavaScripts/lighter/animation";
-
