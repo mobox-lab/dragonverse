6a6a1d687d011d8fc8bcfc45605be1f66b3287a7
Tomori6657 <japancjc123@gmail.com>
2024-05-09 17:48:20 +0800

    feat：update dragon excel and dragon point update

--- dragon-verse/JavaScripts/config/Scene.ts
+++ dragon-verse/JavaScripts/config/Scene.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["id","name","name_str","foreshow","bornAreaId","sceneEnvId","areaIds","capacity"],["","Language","","","","","",""],[1,"Main_Scene_Name","主场景",null,0,1,[9],3],[2,"Main_Scene_Name1","涌动之水",null,22,2,[15],7],[3,"Main_Scene_Name2","火炎地狱",null,23,3,[21],7],[4,"Main_Scene_Name3","天空树",null,24,4,[18,19],7],[5,"Main_Scene_Name4","荒野边疆",null,25,5,[16],7],[6,"Main_Scene_Name5","赐福天光",null,26,6,[20],7],[7,"Main_Scene_Name6","永眠之暗",null,27,7,[17],10],[8,null,null,null,28,0,null,0]];
+const EXCELDATA:Array<Array<any>> = [["id","name","name_str","foreshow","bornAreaId","sceneEnvId","areaIds","capacity"],["","Language","","","","","",""],[1,"Main_Scene_Name","主场景",null,0,1,[9],3],[2,"Main_Scene_Name1","涌动之水",null,22,2,[21],7],[3,"Main_Scene_Name2","火炎地狱",null,23,3,[16],7],[4,"Main_Scene_Name3","天空树",null,24,4,[18,19],7],[5,"Main_Scene_Name4","荒野边疆",null,25,5,[20],7],[6,"Main_Scene_Name5","赐福天光",null,26,6,[15],7],[7,"Main_Scene_Name6","永眠之暗",null,27,7,[17],10],[8,null,null,null,28,0,null,0]];
 export interface ISceneElement extends IElementBase{
  	/**undefined*/
 	id:number
