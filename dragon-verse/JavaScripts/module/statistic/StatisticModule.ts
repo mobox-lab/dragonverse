ade7c9bbafd001055b236dbaf43066741f52d00a
joji-mb <545633740@qq.com>
2024-05-09 10:32:15 +0800

    feat: add StatisticModule

---
+++ dragon-verse/JavaScripts/module/statistic/StatisticModule.ts
@@ -0,0 +1,193 @@
+import { JModuleC, JModuleData, JModuleS } from "../../depend/jibu-module/JModule";
+import Gtk, { GtkTypes, Regulator } from "../../util/GToolkit";
+import Log4Ts from "../../depend/log4ts/Log4Ts";
+
+export default class StatisticModuleData extends JModuleData {
+    //@Decorator.persistence()
+    //public isSave: bool;
+
+    /**
+     * 玩家进入游戏计数.
+     * @type {number}
+     */
+    @Decorator.persistence()
+    playerEnteredCounterS: number;
+
+    /**
+     * 玩家游戏时长.
+     * @type {number}
+     */
+    @Decorator.persistence()
+    playerElapsedTimeS: number;
+
+    @Decorator.persistence()
+    maybeCheatingReason: string[];
+}
+
+interface PlayerIntervalData {
+    elapsedTime: number;
+}
+
+/**
+ * Statistic Module 全局玩家统计模块.
+ *
+ * @desc # Statistic Module
+ * @desc
+ * @desc ## 统计字段规范
+ * @desc
+ * @desc 统计字段的来源可能是 客户端 C 、服务端 S 或 双端 CS.
+ * @desc
+ * @desc - **客户端 C** 仅在客户端 C 影响存储. 禁止敏感数据.
+ * @desc - **服务端 S** 仅在客户端 S 影响存储. 允许敏感数据
+ * @desc - **双端 CS** 取劣值的值影响存储. 允许敏感数据
+ * @desc      - 劣质：取作弊时不会倾向于修改的方向的值.
+ * @desc ---
+ * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
+ * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
+ * ⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄
+ * ⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄
+ * ⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+ * @author LviatYi
+ * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+ * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
+ */
+export class StatisticModuleC extends JModuleC<StatisticModuleS, StatisticModuleData> {
+//#region Member
+    private _eventListeners: EventListener[] = [];
+
+    private _elapsedTime: number = 0;
+
+    private _autoReportRegulator = new Regulator(GtkTypes.Interval.PerMin * 5);
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region MetaWorld Event
+    protected onAwake(): void {
+        super.onAwake();
+
+//#region Inner Member init
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+    }
+
+    protected onJStart(): void {
+//#region Member init
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Event Subscribe
+        // this._eventListeners.push(Event.addLocalListener(EventDefine.EVENT_NAME, CALLBACK));
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+    }
+
+    protected onUpdate(dt: number): void {
+        super.onUpdate(dt);
+        this._elapsedTime += dt;
+
+        if (this._autoReportRegulator.request()) {
+            this.reportPlayerDataInterval();
+        }
+    }
+
+    protected onEnterScene(sceneType: number): void {
+        super.onEnterScene(sceneType);
+    }
+
+    protected onDestroy(): void {
+        super.onDestroy();
+//#region Event Unsubscribe
+        this._eventListeners.forEach(value => value.disconnect());
+//#endregion ------------------------------------------------------------------------------------------
+    }
+
+    protected onExecute(type: number, ...params: any[]): void {
+        super.onExecute(type, ...params);
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Method
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Net Method
+    private reportPlayerDataInterval() {
+        this.server.net_reportPlayerDataInterval({elapsedTime: this._elapsedTime});
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
+
+export class StatisticModuleS extends JModuleS<StatisticModuleC, StatisticModuleData> {
+//#region Member
+    private _eventListeners: EventListener[] = [];
+
+    /**
+     * 玩家进入时间.
+     * @type {Map<number, [number, number]>}
+     */
+    private _playerEnteredTime: Map<number, number> = new Map();
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region MetaWorld Event
+    protected onAwake(): void {
+        super.onAwake();
+    }
+
+    protected onJStart(): void {
+//#region Member init
+//#endregion ------------------------------------------------------------------------------------------ 
+
+//#region Event Subscribe
+        // this._eventListeners.push(Event.addLocalListener(EventDefine.EVENT_NAME, CALLBACK));
+//#endregion ------------------------------------------------------------------------------------------
+    }
+
+    protected onUpdate(dt: number): void {
+        super.onUpdate(dt);
+    }
+
+    protected onDestroy(): void {
+        super.onDestroy();
+//#region Event Unsubscribe
+        this._eventListeners.forEach(value => value.disconnect());
+//#endregion ------------------------------------------------------------------------------------------
+    }
+
+    protected onExecute(type: number, ...params: any[]): void {
+        super.onExecute(type, ...params);
+    }
+
+    protected onPlayerJoined(player: Player): void {
+        super.onPlayerJoined(player);
+    }
+
+    protected onPlayerEnterGame(player: Player): void {
+        super.onPlayerEnterGame(player);
+
+        this._playerEnteredTime.set(player.playerId, Date.now());
+        ++this.getPlayerData(player).playerEnteredCounterS;
+    }
+
+    protected onPlayerLeft(player: Player): void {
+        super.onPlayerLeft(player);
+        this.getPlayerData(player).playerElapsedTimeS += Date.now() - this._playerEnteredTime.get(player.playerId);
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Method
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Net Method
+    public net_reportPlayerDataInterval(data: PlayerIntervalData) {
+        let playerId = this.currentPlayerId;
+        let enteredTime = this._playerEnteredTime.get(playerId);
+        if (Math.abs(Date.now() - enteredTime - data.elapsedTime) > GtkTypes.Interval.PerHour) {
+            let playerMaybeCheatingReason = [`player maybe cheating.`,
+                `playerId: ${playerId}`,
+                `client elapsedTime: ${data.elapsedTime}`,
+                `server elapsedTime: ${Date.now() - enteredTime}`];
+            this.getPlayerData(playerId).maybeCheatingReason.push(playerMaybeCheatingReason.join());
+            Log4Ts.warn(StatisticModuleS, ...playerMaybeCheatingReason);
+        }
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
