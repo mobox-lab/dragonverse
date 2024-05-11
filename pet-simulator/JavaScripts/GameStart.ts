c3d146ad83368544a9fafbdbf7e8cc2ce907f432
Tomori6657 <japancjc123@gmail.com>
2024-05-11 13:31:34 +0800

    fix: pet cursor bug

--- pet-simulator/JavaScripts/GameStart.ts
+++ pet-simulator/JavaScripts/GameStart.ts
@@ -157,7 +157,9 @@ export default class GameStart extends mw.Script {
                 ui.text = langueConfig.Value;
             });
             ChatService.asyncEnableChatWindow(false);
+            InputUtil.mouseLockOptionEnabled = false;
             InputUtil.isCursorVisible = true;
+            InputUtil.isLockMouse = false;
         }
 
         this.useUpdate = true;
--- pet-simulator/JavaScripts/modules/Input/InputModule.ts
+++ pet-simulator/JavaScripts/modules/Input/InputModule.ts
@@ -17,7 +17,7 @@ class resTime {
 export class InputModuleC extends ModuleC<InputModuleS, null> {
 
     public onLineTraceAC: Action2<resourceScript, boolean> = new Action2<resourceScript, boolean>();
-    private touch: mw.TouchInput = null;
+    // private touch: mw.TouchInput = null;
     private touchNum: number = 0;
     private oldPointSize: number = 0;
     private camera: Camera = null;
@@ -52,14 +52,14 @@ export class InputModuleC extends ModuleC<InputModuleS, null> {
     }
 
     private startLineTrace() {
-        this.touch = new mw.TouchInput();
-        this.touch.onTouchBegin.add(this.touchEvent.bind(this));
-        this.touch.onTouchEnd.add((idnex, loc, touchType) => {
+        // this.touch = n
+        InputUtil.onTouchBegin(this.touchEvent.bind(this));
+        InputUtil.onTouchEnd((index, loc, touchType) => {
             let v3 = InputUtil.convertScreenLocationToWorldSpace(loc.x, loc.y);
             let forVect = v3.worldDirection;
             let tarLoc = v3.worldPosition.clone().add(forVect.multiply(GlobalData.LineTrace.lineTraceDistance));
             let sc = this.isHitObject(v3, tarLoc);
-            let hitSc = this.hitObjects.get(idnex);
+            let hitSc = this.hitObjects.get(index);
             if (sc && hitSc && hitSc.res == sc) {
                 this.sendLineTraceAC();
                 let time = Date.now() - hitSc.time;
@@ -123,49 +123,49 @@ export class InputModuleC extends ModuleC<InputModuleS, null> {
     }
 
     // 设置手指事件
-    private setFinger() {
-        // 使用touchNum记录当前屏幕的触摸点数量
-        this.touchNum = 0
-        //开始触摸（包括第一个和第二个触摸点）
-        this.touch.onTouchBegin.add(() => {
-            this.touchNum++;
-            if (this.touchNum < 2) return;
-            mw.UIService.getUI(P_HudUI).mTouchPad.removeObject();
-            //使用oldPointSize记录两个触摸点的初始距离
-            let touchPoint = this.touch.getTouchVectorArray();
-            this.oldPointSize = touchPoint[0].subtract(touchPoint[1]).length;
-        });
+    // private setFinger() {
+    //     // 使用touchNum记录当前屏幕的触摸点数量
+    //     this.touchNum = 0
+    //     //开始触摸（包括第一个和第二个触摸点）
+    //     this.touch.onTouchBegin.add(() => {
+    //         this.touchNum++;
+    //         if (this.touchNum < 2) return;
+    //         mw.UIService.getUI(P_HudUI).mTouchPad.removeObject();
+    //         //使用oldPointSize记录两个触摸点的初始距离
+    //         let touchPoint = this.touch.getTouchVectorArray();
+    //         this.oldPointSize = touchPoint[0].subtract(touchPoint[1]).length;
+    //     });
 
-        //触摸点移动（包括第一个和第二个触摸点）
-        this.touch.onTouchMove.add(() => {
-            if (this.touchNum < 2) return
-            //计算两个触摸点移动过程中的最新距离
-            let touchPoint = this.touch.getTouchVectorArray();
-            let newPointSize = touchPoint[0].subtract(touchPoint[1]).length;
-            //计算初始距离和最新距离之差
-            let char = this.localPlayer.character;
-            let distance = newPointSize - this.oldPointSize;
-            //使用length记录当前弹簧臂长度，也就是摄像机距离，加上或者减去两个触摸点初始距离和最新距离之差
-            let length = Camera.currentCamera.springArm.length;
-            let addLength = (distance > 0 ? -1 : distance < 0 ? 1 : 0) * 1 * Math.abs(distance);
-            addLength *= GlobalData.Camera.zoomRate;
-            length += addLength;
-            length = Math.max(length, GlobalData.Camera.minLength);
-            length = Math.min(length, GlobalData.Camera.maxLength);
-            //应用length记录的弹簧臂长度，并且用oldPointSize再次记录两个触摸点的初始距离
-            Camera.currentCamera.springArm.length = length;
-            this.oldPointSize = newPointSize;
-        });
+    //     //触摸点移动（包括第一个和第二个触摸点）
+    //     this.touch.onTouchMove.add(() => {
+    //         if (this.touchNum < 2) return
+    //         //计算两个触摸点移动过程中的最新距离
+    //         let touchPoint = this.touch.getTouchVectorArray();
+    //         let newPointSize = touchPoint[0].subtract(touchPoint[1]).length;
+    //         //计算初始距离和最新距离之差
+    //         let char = this.localPlayer.character;
+    //         let distance = newPointSize - this.oldPointSize;
+    //         //使用length记录当前弹簧臂长度，也就是摄像机距离，加上或者减去两个触摸点初始距离和最新距离之差
+    //         let length = Camera.currentCamera.springArm.length;
+    //         let addLength = (distance > 0 ? -1 : distance < 0 ? 1 : 0) * 1 * Math.abs(distance);
+    //         addLength *= GlobalData.Camera.zoomRate;
+    //         length += addLength;
+    //         length = Math.max(length, GlobalData.Camera.minLength);
+    //         length = Math.min(length, GlobalData.Camera.maxLength);
+    //         //应用length记录的弹簧臂长度，并且用oldPointSize再次记录两个触摸点的初始距离
+    //         Camera.currentCamera.springArm.length = length;
+    //         this.oldPointSize = newPointSize;
+    //     });
 
-        //结束触摸点离开（包括第一个和第二个触摸点）
-        this.touch.onTouchEnd.add(() => {
-            this.touchNum--;
-            //当最后一个触摸点离开屏幕时，重新挂载UI对象中的摄像机滑动区
-            if (this.touchNum < 1) {
-                mw.UIService.getUI(P_HudUI).rootCanvas.addChild(mw.UIService.getUI(P_HudUI).mTouchPad);
-            }
-        })
-    }
+    //     //结束触摸点离开（包括第一个和第二个触摸点）
+    //     this.touch.onTouchEnd.add(() => {
+    //         this.touchNum--;
+    //         //当最后一个触摸点离开屏幕时，重新挂载UI对象中的摄像机滑动区
+    //         if (this.touchNum < 1) {
+    //             mw.UIService.getUI(P_HudUI).rootCanvas.addChild(mw.UIService.getUI(P_HudUI).mTouchPad);
+    //         }
+    //     })
+    // }
 
     private isFirst: boolean = false;
     private curLenth: number = 0;
