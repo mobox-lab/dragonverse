f1a3cc6cb48004c1f02794a8de265d8effe7d86a
joji-mb <545633740@qq.com>
2024-05-11 18:06:48 +0800

    feat: add KOM for DPC

--- dragon-verse/JavaScripts/depend/dialogify/dialogue-panel-controller/DialoguePanelController.ts
+++ dragon-verse/JavaScripts/depend/dialogify/dialogue-panel-controller/DialoguePanelController.ts
@@ -6,6 +6,7 @@ import ADialogifyConfigReader, {
 } from "../dialogify-config-reader/ADialogifyConfigReader";
 import DialoguePanel_Generate from "../../../ui-generate/dialogue/DialoguePanel_generate";
 import InteractionNode from "../../../ui/dialogue/InteractionNode";
+import KeyOperationManager from "../../../controller/key-operation-manager/KeyOperationManager";
 
 export default class DialoguePanelController extends ADialoguePanelController<
     DialoguePanel_Generate,
@@ -22,6 +23,12 @@ export default class DialoguePanelController extends ADialoguePanelController<
     public constructor() {
         super();
         this.registerPanel(UIService.create(DialoguePanel_Generate));
+
+        KeyOperationManager.getInstance().onKeyDown(
+            this._panel,
+            mw.Keys.Enter,
+            () => this._panel.btnNext.onClicked.broadcast(),
+        );
     }
 
     protected get interactorItemConstructor(): new () => InteractionNode {
