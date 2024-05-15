fa567b0c81276b8a536ab314e3f3e106b6952af4
joji-mb <545633740@qq.com>
2024-05-15 11:42:28 +0800

    feat: add alt support

--- battleworld/JavaScripts/controller/MouseLockController.ts
+++ battleworld/JavaScripts/controller/MouseLockController.ts
@@ -7,7 +7,6 @@
  * @Description  : 鼠标锁定管理器
  */
 
-
 import KeyOperationManager from "./key-operation-manager/KeyOperationManager";
 
 export class MouseLockController {
@@ -36,15 +35,20 @@ export class MouseLockController {
         this._needLeftMouseButton = null;
     }
 
-
     needMouseUnlock() {
         this._needMouseUnlock.push(true);
         InputUtil.isLockMouse = false;
         //录一次就行
         if (this._needMouseUnlock.length === 1) {
             //为了防止松开alt会导致鼠标锁定
-            KeyOperationManager.getInstance().onKeyUp(null, Keys.LeftAlt, () => { });
-            KeyOperationManager.getInstance().onKeyUp(null, Keys.RightAlt, () => { });
+            KeyOperationManager.getInstance().onKeyUp(null, Keys.LeftAlt, () => {
+            });
+            KeyOperationManager.getInstance().onKeyUp(null, Keys.RightAlt, () => {
+            });
+            KeyOperationManager.getInstance().onKeyUp(null, Keys.LeftCommand, () => {
+            });
+            KeyOperationManager.getInstance().onKeyUp(null, Keys.RightCommand, () => {
+            });
             if (this._needLeftMouseButton) this._needLeftMouseButton.removeKey(Keys.LeftMouseButton);
         }
     }
@@ -55,6 +59,8 @@ export class MouseLockController {
             InputUtil.isLockMouse = true;
             KeyOperationManager.getInstance().unregisterKey(null, Keys.LeftAlt);
             KeyOperationManager.getInstance().unregisterKey(null, Keys.RightAlt);
+            KeyOperationManager.getInstance().unregisterKey(null, Keys.LeftCommand);
+            KeyOperationManager.getInstance().unregisterKey(null, Keys.RightCommand);
             if (this._needLeftMouseButton) this._needLeftMouseButton.addKey(Keys.LeftMouseButton);
         }
     }
