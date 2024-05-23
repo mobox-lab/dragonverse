9fc2b920b5436b879eb84233d016136f58ddef26
Tomori6657 <japancjc123@gmail.com>
2024-05-23 17:08:57 +0800

    feat: update config
    更新多语言和地标配置

--- dragon-verse/JavaScripts/config/LandMark.ts
+++ dragon-verse/JavaScripts/config/LandMark.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["id","uiName","description"],["","","Language"],[1,"imgSymbol6","Maptext006"],[2,"imgSymbol7","Maptext007"],[3,"imgSymbol1","Maptext001"],[4,"imgSymbol2","Maptext002"],[5,"imgSymbol3","Maptext003"],[6,"imgSymbol4","Maptext004"],[7,"imgSymbol5","Maptext005"],[8,"imgSymbol8","Maptext008"]];
+const EXCELDATA:Array<Array<any>> = [["id","uiName","description"],["","","Language"],[1,"imgSymbol6","Maptext006"],[2,"imgSymbol7","Maptext007"]];
 export interface ILandMarkElement extends IElementBase{
  	/**id*/
 	id:number
