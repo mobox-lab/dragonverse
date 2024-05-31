1e5bfce7e4ec763c1a478e16d2072984bd978f3d
Tomori6657 <japancjc123@gmail.com>
2024-05-31 14:39:21 +0800

    feat：update multiplekill ts

--- battleworld/JavaScripts/config/MultipleKill.ts
+++ battleworld/JavaScripts/config/MultipleKill.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["ID","Factor"],["",""],[1,1],[2,1.2],[3,1.4],[4,1.6],[5,2],[6,2.2],[7,2.4],[8,2.6],[9,2.8],[10,3]];
+const EXCELDATA:Array<Array<any>> = [["ID","Factor"],["",""],[1,1],[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1],[9,1],[10,1]];
 export interface IMultipleKillElement extends IElementBase{
  	/**ID(即连杀数量)*/
 	ID:number
