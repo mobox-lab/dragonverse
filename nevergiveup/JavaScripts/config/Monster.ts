0c0e3a09fb478e76eb29dfdf744632040759f39d
Tomori6657 <japancjc123@gmail.com>
2024-07-12 15:30:18 +0800

    feat：update monseter vertigoDelay

--- nevergiveup/JavaScripts/config/Monster.ts
+++ nevergiveup/JavaScripts/config/Monster.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["id","types","name","nameCN","elementTy","guid","walkAnim","hp","speed","underattack","attackproportion","goldAmount","escapeDamage","buffSlot","buffSlotRelativePos","hurtAnim","hurtAnimDur","deadAnim","deadAnimDur","armor","magicResist","buff"],["","","Language","","","","","","","","","","","","","","","","","","",""],[1001,null,"Monster_name_1","丧尸",1,"268048","268567",550,105,"107535",1,5,25,23,new mw.Vector(0,0,150),"97861",0.3,"285139",1.5,200,200,[5002]],[1002,[1],"Monster_name_2","快速丧尸",2,"174947","122498",40,200,"107535",1,5,25,23,new mw.Vector(0,0,150),"97861",0.3,"285139",1.5,200,200,null],[1003,[5],"Monster_name_3","僵尸",3,"146185","122549",500,75,"107535",1,100,500,23,new mw.Vector(0,0,150),null,0.3,"20291",1,200,200,null],[1004,[2],"Monster_name_4","飞行",4,"151835","164988",30,150,"107535",1,5,25,23,new mw.Vector(0,0,150),"145952",0.3,"285395",1,200,200,null],[1005,[3],"Monster_name_5","护士",5,"174911","97860",40,150,"107535",1,5,25,23,new mw.Vector(0,0,150),"97861",0.3,"285139",1.5,200,200,null],[1006,[5],"Monster_name_6","铁钩怪人",6,"145900","122498",500,75,"107535",1,100,500,23,new mw.Vector(0,0,150),"97861",0.3,"285139",1.5,200,200,null],[1007,null,"Monster_name_1","龙娘测试",6,"268048","268567",50,50,"107535",1,5,20,23,new mw.Vector(0,0,150),"97861",0.3,"281693",1.5,200,200,null]];
+const EXCELDATA:Array<Array<any>> = [["id","types","name","nameCN","elementTy","guid","walkAnim","hp","speed","underattack","attackproportion","goldAmount","escapeDamage","buffSlot","buffSlotRelativePos","hurtAnim","hurtAnimDur","deadAnim","deadAnimDur","vertigoDelay","armor","magicResist","buff"],["","","Language","","","","","","","","","","","","","","","","","","","",""],[1001,null,"Monster_name_1","丧尸",1,"268048","268567",550,105,"107535",1,5,25,23,new mw.Vector(0,0,150),"97861",0.3,"285139",1.5,3,200,200,[5002]],[1002,[1],"Monster_name_2","快速丧尸",2,"174947","122498",40,200,"107535",1,5,25,23,new mw.Vector(0,0,150),"97861",0.3,"285139",1.5,3,200,200,null],[1003,[5],"Monster_name_3","僵尸",3,"146185","122549",500,75,"107535",1,100,500,23,new mw.Vector(0,0,150),null,0.3,"20291",1,3,200,200,null],[1004,[2],"Monster_name_4","飞行",4,"151835","164988",30,150,"107535",1,5,25,23,new mw.Vector(0,0,150),"145952",0.3,"285395",1,3,200,200,null],[1005,[3],"Monster_name_5","护士",5,"174911","97860",40,150,"107535",1,5,25,23,new mw.Vector(0,0,150),"97861",0.3,"285139",1.5,3,200,200,null],[1006,[5],"Monster_name_6","铁钩怪人",6,"145900","122498",500,75,"107535",1,100,500,23,new mw.Vector(0,0,150),"97861",0.3,"285139",1.5,3,200,200,null],[1007,null,"Monster_name_1","龙娘测试",6,"268048","268567",50,50,"107535",1,5,20,23,new mw.Vector(0,0,150),"97861",0.3,"281693",1.5,3,200,200,null]];
 export interface IMonsterElement extends IElementBase{
  	/**怪物id*/
 	id:number
@@ -39,6 +39,8 @@ export interface IMonsterElement extends IElementBase{
 	deadAnim:string
 	/**死亡动画持续时间*/
 	deadAnimDur:number
+	/**眩晕后的内置CD（从怪物受到眩晕buff时，应用该列计时，时间内怪物不会再次受到眩晕buff影响）*/
+	vertigoDelay:number
 	/**初始护甲*/
 	armor:number
 	/**初始魔抗*/
