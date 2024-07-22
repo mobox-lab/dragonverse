eb27c226e99b904d3b3c56c94057009d02d9aea9
Tomori6657 <japancjc123@gmail.com>
2024-07-22 18:43:24 +0800

    feat：update lightdragon 1 and 4

--- nevergiveup/JavaScripts/config/LevelEffect.ts
+++ nevergiveup/JavaScripts/config/LevelEffect.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["id","effectGuid","slot","nameCN","circulate","location"],["","","","","",""],[1,"E718B09E4408CE5534779780E5365B64",10,"炫酷的特效",true,[10,10,100]],[2,"E141AC6246FA01BC41E98BB4D9423F80E141AC6246FA01BC41E98BB4D9423F80",23,null,true,[0,0,150]],[3,"E0D79BB64C2DCD08AEC54388C11A6AE7E0D79BB64C2DCD08AEC54388C11A6AE7",23,null,true,[0,0,150]]];
+const EXCELDATA:Array<Array<any>> = [["id","effectGuid","slot","nameCN","circulate","location"],["","","","","",""],[1,"E718B09E4408CE5534779780E5365B64",10,"炫酷的特效",true,[10,10,100]],[2,"E141AC6246FA01BC41E98BB4D9423F80",23,null,true,[0,0,100]],[3,"E0D79BB64C2DCD08AEC54388C11A6AE7",23,null,true,[0,0,100]],[4,"1D729A16497C24D2C4B6A9A247F09AE9",23,null,true,[0,0,100]],[5,"C030E576410EBE92DF5FAB83FAFD859A",23,null,true,[-60,0,70]]];
 export interface ILevelEffectElement extends IElementBase{
  	/**特效id*/
 	id:number
