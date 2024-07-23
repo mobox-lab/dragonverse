59fe26d7deb499a448ebc69513018215abc8c2c6
C-Green-Code <shallowgreenjob@gmail.com>
2024-07-23 15:59:30 +0800

    feat: add talentS check

--- nevergiveup/JavaScripts/Modules/talent/TalentModuleS.ts
+++ nevergiveup/JavaScripts/Modules/talent/TalentModuleS.ts
@@ -4,6 +4,7 @@ import TalentModuleData from "./TalentModuleData";
 import { PlayerModuleS } from "../PlayerModule/PlayerModuleS";
 import Player = mw.Player;
 import { GameConfig } from "../../config/GameConfig";
+import { ITalentTreeElement } from "../../config/TalentTree";
 
 export default class TalentModuleS extends JModuleS<TalentModuleC, TalentModuleData> {
     private _playS: PlayerModuleS;
@@ -26,9 +27,24 @@ export default class TalentModuleS extends JModuleS<TalentModuleC, TalentModuleD
         return data.getTalentIndex(id);
     }
 
+    /**
+     * 检测前置天赋是否解锁
+     * @param {string} userId
+     * @param {ITalentTreeElement} talent
+     * @returns {boolean}
+     * @private
+     */
+    private checkTalentCanActive(userId: string, talent: ITalentTreeElement): boolean {
+        if (!talent.frontTalent?.length) return true;
+        const frontLevels = talent.frontTalent.map(frontId => this.getPlayerTalentIndex(userId, frontId));
+        return frontLevels.every(n => n > 0);
+    }
+
     public async net_updateTalentLevel(id: number) {
         const player = this.currentPlayer;
         const talent = GameConfig.TalentTree.getElement(id);
+        const canActive = this.checkTalentCanActive(player.userId, talent);
+        if (!canActive) return false;
         const level = this.getPlayerTalentIndex(player.userId, id);
         const goldCost = [talent.cost[0][0], talent.cost[0][level + 1]];
         const techCost = [talent.cost[1][0], talent.cost[1][level + 1]];
