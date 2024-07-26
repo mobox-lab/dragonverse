f066acb8506a67b58232b2470eb3b05a1ab93e3a
C-Green-Code <shallowgreenjob@gmail.com>
2024-07-26 12:04:15 +0800

    feat: dragon buff

--- nevergiveup/JavaScripts/Modules/talent/TalentUtils.ts
+++ nevergiveup/JavaScripts/Modules/talent/TalentUtils.ts
@@ -1,5 +1,6 @@
 import Utils from "../../Utils";
 import { GameConfig } from "../../config/GameConfig";
+import { AuthModuleC, AuthModuleS, UserDragonRespData } from "../auth/AuthModule";
 import TalentModuleC from "./TalentModuleC";
 import TalentModuleS from "./TalentModuleS";
 
@@ -37,10 +38,61 @@ export default class TalentUtils {
         return index;
     }
 
+    public static getDragonIndex(id: number): number {
+        const dragonData = ModuleService.getModule(AuthModuleC).dragonData;
+        const index = this.getDragonIndexByData(dragonData, id);
+        return index;
+    }
+
+    public static getDragonIndexByData(dragonData: UserDragonRespData | undefined, id: number) {
+        let index = 0;
+        if (dragonData && Array.isArray(dragonData?.DragonPalList)) {
+            let dragons = [];
+            if (id === 2001) {
+                // 火龙娘
+                dragons = dragonData.DragonPalList.filter((d) => d.dragonPalId > 40 && d.dragonPalId < 50);
+            } else if (id === 2002) {
+                // 水龙娘
+                dragons = dragonData.DragonPalList.filter((d) => d.dragonPalId > 30 && d.dragonPalId < 40);
+            } else if (id === 2003) {
+                // 土龙娘
+                dragons = dragonData.DragonPalList.filter((d) => d.dragonPalId > 60 && d.dragonPalId < 70);
+            } else if (id === 2004) {
+                // 木龙娘
+                dragons = dragonData.DragonPalList.filter((d) => d.dragonPalId > 50 && d.dragonPalId < 60);
+            } else if (id === 2005) {
+                // 光龙娘
+                dragons = dragonData.DragonPalList.filter((d) => d.dragonPalId > 10 && d.dragonPalId < 20);
+            } else if (id === 2006) {
+                // 暗龙娘
+                dragons = dragonData.DragonPalList.filter((d) => d.dragonPalId > 20 && d.dragonPalId < 30);
+            } else {
+                return index;
+            }
+
+            const ids = dragons.map((item) => item.dragonPalId);
+            const uniqueIds = [...new Set(ids)];
+            if (uniqueIds.length >= 6) {
+                index = 3;
+            } else if (uniqueIds.length >= 3) {
+                index = 2;
+            } else if (uniqueIds.length >= 1) {
+                index = 1;
+            }
+            return index;
+        }
+        return index;
+    }
+
     public static getModuleCRunesValueById(id: number) {
-        // todo 龙娘的id
         if (id > 2000) {
-            return 0;
+            const index = this.getDragonIndex(id);
+            if (index > 0) {
+                const value = this.getRunesConfigByKey(id, index - 1);
+                return value;
+            } else {
+                return 0;
+            }
         }
         const index = this.getModuleCRunesIndexById(id);
         // 无尽天赋
@@ -61,12 +113,18 @@ export default class TalentUtils {
         }
     }
 
-    public static getModuleSRunesValueById(id: number, userId: string) {
-        // todo 龙娘的id
+    public static async getModuleSRunesValueById(id: number, player: Player) {
         if (id > 2000) {
-            return 0;
+            const dragonData = await ModuleService.getModule(AuthModuleS).queryUserDragon(player.playerId);
+            const index = this.getDragonIndexByData(dragonData, id);
+            if (index > 0) {
+                const value = this.getRunesConfigByKey(id, index - 1);
+                return value;
+            } else {
+                return 0;
+            }
         }
-        const index = this.getModuleSRunesIndexById(id, userId);
+        const index = this.getModuleSRunesIndexById(id, player.userId);
         // 无尽天赋
         if (id === 1047 || id === 1048 || id === 1049) {
             if (index > 0) {
