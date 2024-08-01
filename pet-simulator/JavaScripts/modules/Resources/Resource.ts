ae97d75862b944ab8b8cd9a5e0a32d2be2088ae6
joji-mb <545633740@qq.com>
2024-08-01 19:17:28 +0800

    fix: remove delay

--- pet-simulator/JavaScripts/modules/Resources/Resource.ts
+++ pet-simulator/JavaScripts/modules/Resources/Resource.ts
@@ -614,9 +614,6 @@ export default class ResourceScript extends mw.Script {
 
     /**创建默认资源*/
     public async createDefaultObj(): Promise<boolean> {
-        if (SystemUtil.isClient()) {
-            await TimeUtil.delaySecond(MathUtil.randomFloat(0, 1));
-        } 
         if (!this.cfg || this.isStart || this.resObj != null || this.curHp <= 0) return false;
         this.isStart = true;
         this.resObj = await Resource.instance.getResource(this.cfgId);
