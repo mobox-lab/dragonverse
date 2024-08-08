93eeff1060cf6ef73e03cab6b5a3829fc01cd1e4
Tomori6657 <japancjc123@gmail.com>
2024-08-08 15:02:59 +0800

    feat: update TimeReward
    提高在线礼包奖励

--- pet-simulator/JavaScripts/config/TimeReward.ts
+++ pet-simulator/JavaScripts/config/TimeReward.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["id","RewardName","Time","PicICON","RewardArr","buff","buffTime"],["","","","","","",""],[1,"礼包1",180,174803,[500,100,0,0],[0],[300,600]],[2,"礼包2",300,174803,[1000,200,0,0],[0],[300,600]],[3,"礼包3",480,174824,[2000,300,0,0],[0],[300,300]],[4,"礼包4",600,174824,[3500,400,0,0],[0],[600]]];
+const EXCELDATA:Array<Array<any>> = [["id","RewardName","Time","PicICON","RewardArr","buff","buffTime"],["","","","","","",""],[1,"礼包1",60,174803,[5000,2000,0,0],[0],[300,600]],[2,"礼包2",180,174803,[5500,2200,0,0],[0],[300,600]],[3,"礼包3",360,174824,[6000,2400,0,0],[0],[300,300]],[4,"礼包4",540,174824,[7000,3000,0,0],[0],[600]]];
 export interface ITimeRewardElement extends IElementBase{
  	/**ID*/
 	id:number
