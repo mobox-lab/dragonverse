082409152dc3807703421ccdaf349947115391e7
joji-mb <545633740@qq.com>
2024-08-12 15:50:15 +0800

    feat: bless ui

--- nevergiveup/JavaScripts/UI/Dragon/BlessItemUI.ts
+++ nevergiveup/JavaScripts/UI/Dragon/BlessItemUI.ts
@@ -5,18 +5,19 @@ import BlessItem_Generate from "../../ui-generate/Bless/BlessItem_generate";
 import { Yoact } from "../../depend/yoact/Yoact";
 import bindYoact = Yoact.bindYoact;
 import { GlobalData } from "../../const/GlobalData";
+import Gtk from "gtoolkit";
 
 export default class BlessItemUI extends BlessItem_Generate implements IScrollViewItem<DragonBlessListUnique>{
     //#region IScrollViewItem
     bindData(data: DragonBlessListUnique): void {
         bindYoact(() => {
-            // this.img_Icon.renderOpacity = data.cnt ? 1 : 0.2;
-            // this.textNumber_Count.text = data.cnt ? "x" + data.cnt.toString() : "";
+            this.rootCanvas.renderOpacity = data.cnt ? 1 : 0.4;
+            Gtk.trySetVisibility(this.txt_Percent, data.cnt ? mw.SlateVisibility.Visible : mw.SlateVisibility.Collapsed);
         });
         const dragons = GameConfig.Dragon.getAllElement();
         const dragon = dragons?.find((d) => d.dragonPalId === data.id);
         this.img_Icon.imageGuid = dragon?.imgGuid;
-        this.img_IconBg.imageGuid = GlobalData.Shop.shopItemBgGuid[(dragon?.tdElemetType || 1) - 1];
+        this.img_IconBg.imageGuid = GlobalData.Bless.blessItemBgGuid[(dragon?.tdElemetType || 1) - 1];
         this.img_Corner.imageGuid = GlobalData.Shop.shopItemCornerIconGuid[(dragon?.tdElemetType || 1) - 1];
     }
 
