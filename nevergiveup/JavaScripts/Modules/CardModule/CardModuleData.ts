eafa6f94cce4b4391b4f0264931417725edacc50
C-Green-Code <shallowgreenjob@gmail.com>
2024-08-27 16:20:56 +0800

    fix: equip

--- nevergiveup/JavaScripts/Modules/CardModule/CardModuleData.ts
+++ nevergiveup/JavaScripts/Modules/CardModule/CardModuleData.ts
@@ -51,7 +51,7 @@ export default class CardModuleData extends Subdata {
     protected onDataInit(): void {
         console.error("数据初始化");
         if (this._unlockCards == undefined) this._unlockCards = [GlobalData.Tower.initUnlockTower];
-        if (this._unlockCards == undefined) this._equipCards = [GlobalData.Tower.initUnlockTower];
+        if (this._equipCards == undefined) this._equipCards = [GlobalData.Tower.initUnlockTower];
     }
 
     public addUnlockCard(cardID: number) {
