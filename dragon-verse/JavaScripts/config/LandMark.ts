a11c4cd7056bb843f5ab1f4a9614281a9c8354a9
Tomori6657 <japancjc123@gmail.com>
2024-09-10 10:17:54 +0800

    feat：update map panel point

--- dragon-verse/JavaScripts/config/LandMark.ts
+++ dragon-verse/JavaScripts/config/LandMark.ts
@@ -1,5 +1,5 @@
 import { ConfigBase, IElementBase } from "./ConfigBase";
-const EXCELDATA:Array<Array<any>> = [["id","uiName","description"],["","","Language"],[1,"imgSymbol6","Maptext006"],[2,"imgSymbol7","Maptext007"],[3,"imgSymbol4","Maptext009"]];
+const EXCELDATA:Array<Array<any>> = [["id","uiName","description"],["","","Language"],[1,"imgSymbol6","Maptext006"],[2,"imgSymbol7","Maptext007"],[3,"imgSymbol4","Maptext009"],[4,"imgSymbol9","Maptext010"]];
 export interface ILandMarkElement extends IElementBase{
  	/**id*/
 	id:number
