784d7d5fb893be105e1904d30fdd5eb09b1ba568
C-Green-Code <shallowgreenjob@gmail.com>
2024-11-28 16:04:52 +0800

    feat: change wave calculate

--- nevergiveup/JavaScripts/enemy/EnemyBase.ts
+++ nevergiveup/JavaScripts/enemy/EnemyBase.ts
@@ -117,12 +117,15 @@ export class Enemy implements BuffBag {
         let difficlutyMutliplier = stageConfig.difficultyhp;
         let multiplayerMultiplier = GameManager.getMultiplayerMultiplier();
         this.hp = config.hp * difficlutyMutliplier * multiplayerMultiplier * waveMultiplier;
-        this.armor = config.armor || 200;
-        this.magicResist = config.magicResist || 200;
+        const armorPercent = waveConfig?.armor || 1;
+        const magicReductionPercent = waveConfig?.magicReduction || 1;
+        this.armor = (config.armor || 200) * armorPercent;
+        this.magicResist = (config.magicResist || 200) * magicReductionPercent;
         this.hpMax = this.hp;
         this.speed = config.speed;
         this.hurtAmount = 0;
         const escapeDamagePercent = waveConfig?.escapeDamagePercent || 1;
+
         this.escapeDamage = Math.floor(config.escapeDamage * escapeDamagePercent);
         this.goldAmount = config.goldAmount;
         this.name = config.name;
